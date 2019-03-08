200c97f569180a9bbae4f23eccbb768b6b6ce635
Ignacio Cordoba <icordoba@4geeks.co>
2019-03-08 22:21:18 +0000

    Refreshment, visual improvements and modules updates

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
@@ -15,17 +14,6 @@ export class Home extends React.Component {
 				<a href="#" className="btn btn-success">
 					If you see this green button, bootstrap is working
 				</a>
-				<hr />
-				<Link to="/demo">
-					<button className="btn btn-primary">
-						Check the Context in action
-					</button>
-				</Link>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
 			</div>
 		);
 	}

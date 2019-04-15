af16e33c21ad9dcfb052fdfa451fab175c93310b
Alejandro Sanchez <aalejo@gmail.com>
2019-04-15 11:39:17 -0400

    Update appContext.jsx

--- src/js/store/appContext.jsx
+++ src/js/store/appContext.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import getState from "./flux.js";
 
+
+// Don't change, here is where we initialize our context, by default its just going to be Null.
 export const Context = React.createContext(null);
 
-const injectStore = PassedComponent => {
+
+// This function injects the global store to any view/component where you want to use it, we will inject the context to Layout.jsx, you can see it here:
+// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.jsx#L35
+const injectContent = PassedComponent => {
 	class StoreWrapper extends React.Component {
 		constructor(props) {
 			super(props);
+			
+			//this will be passed as the contenxt value
 			this.state = getState({
 				getStore: () => this.state.store,
 				setStore: updatedStore =>
@@ -17,12 +24,17 @@ const injectStore = PassedComponent => {
 		}
 
 		componentDidMount() {
-			// this function is the equivalent to "window.onLoad"
-			// it only run once on the entire application lifetime
-			// you should do your ajax requests here
+			/**
+			 * EDIT THIS!
+			 * This function is the equivalent to "window.onLoad", it only run once on the entire application lifetime
+			 * you should do your ajax requests or fetch api requests here
+			**/
 		}
 
 		render() {
+			// the initial value for the context its not null anymore, but the current state of this component,
+			// the context will have a getStore and setStore functions available then, because they were declared
+			// on the state of this component
 			return (
 				<Context.Provider value={this.state}>
 					<PassedComponent {...this.props} />
@@ -33,4 +45,4 @@ const injectStore = PassedComponent => {
 	return StoreWrapper;
 };
 
-export default injectStore;
+export default injectContent;

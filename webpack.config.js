b53a3462167564218734e0d4e13a0dcf311c5c6b
Alejandro Sanchez <aalejo@gmail.com>
2019-04-26 18:04:13 +0000

    no more public folder

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
     }),
     new PrettierPlugin({
       parser: "babylon",
-      printWidth: 80,             // Specify the length of line that the printer will wrap on.
+      printWidth: 120,             // Specify the length of line that the printer will wrap on.
       tabWidth: 4,                // Specify the number of spaces per indentation-level.
       useTabs: true,              // Indent lines with tabs instead of spaces.
       bracketSpacing: true,

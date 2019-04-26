daacab61df94977412a7e981030096637baea6e1
Alejandro Sanchez <aalejo@gmail.com>
2019-04-26 18:05:09 +0000

    no more public folder

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -22,18 +22,11 @@ export class Demo extends React.Component {
 											<span>Link to: {item.title}</span>
 										</Link>
 										<p style={{ color: item.initial }}>
-											{
-												"Check store/store.js scroll to the actions to see the code "
-											}
+											{"Check store/store.js scroll to the actions to see the code "}
 										</p>
 										<button
 											className="btn btn-success"
-											onClick={() =>
-												actions.changeColor(
-													index,
-													"orange"
-												)
-											}>
+											onClick={() => actions.changeColor(index, "orange")}>
 											Change Color
 										</button>
 									</li>

42fa884ed79fdd1feab9602fed02c6443782e784
jferragut <31458614+jferragut@users.noreply.github.com>
2019-09-27 13:00:20 -0400

    fix vulnerabilities and update layout comp

--- src/js/layout.js
+++ src/js/layout.js
@@ -11,30 +11,27 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 //create your first component
-export class Layout extends React.Component {
-	render() {
+export const Layout = ()=> {
+	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	const basename = process.env.BASENAME || '';
 
-        //the basename is used when your project is published in a subdirectory and not in the root of the domain
-        // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-        const basename = process.env.BASENAME || '';
-
-		return (
-			<div className="d-flex flex-column h-100">
-				<BrowserRouter basename={basename}>
-					<ScrollToTop>
-						<Navbar />
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route path="/demo" component={Demo} />
-							<Route path="/single/:theid" component={Single} />
-							<Route render={() => <h1>Not found!</h1>} />
-						</Switch>
-						<Footer />
-					</ScrollToTop>
-				</BrowserRouter>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="d-flex flex-column h-100">
+			<BrowserRouter basename={basename}>
+				<ScrollToTop>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/demo" component={Demo} />
+						<Route path="/single/:theid" component={Single} />
+						<Route render={() => <h1>Not found!</h1>} />
+					</Switch>
+					<Footer />
+				</ScrollToTop>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default injectContext(Layout);

29d7c51979646d0d61dfff0d33a587a91716bc7c
jferragut <31458614+jferragut@users.noreply.github.com>
2019-09-27 13:33:38 -0400

    convert demo to function, add conditional render

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -5,41 +5,41 @@ import { Context } from "../store/appContext";
 
 import "../../styles/demo.scss";
 
-export class Demo extends React.Component {
-	render() {
-		return (
-			<div className="container">
-				<ul className="list-group">
-					<Context.Consumer>
-						{({ store, actions }) => {
-							return store.demo.map((item, index) => {
-								return (
-									<li
-										key={index}
-										className="list-group-item d-flex justify-content-between"
-										style={{ background: item.background }}>
-										<Link to={"/single/" + index}>
-											<span>Link to: {item.title}</span>
-										</Link>
-										<p style={{ color: item.initial }}>
-											{"Check store/store.js scroll to the actions to see the code "}
-										</p>
-										<button
-											className="btn btn-success"
-											onClick={() => actions.changeColor(index, "orange")}>
-											Change Color
-										</button>
-									</li>
-								);
-							});
-						}}
-					</Context.Consumer>
-				</ul>
-				<br />
-				<Link to="/">
-					<button className="btn btn-primary">Back home</button>
-				</Link>
-			</div>
-		);
-	}
-}
+export const Demo = () => (
+	<div className="container">
+		<ul className="list-group">
+			<Context.Consumer>
+				{({ store, actions }) => {
+					return store.demo.map((item, index) => {
+						return (
+							<li
+								key={index}
+								className="list-group-item d-flex justify-content-between"
+								style={{ background: item.background }}>
+								<Link to={"/single/" + index}>
+									<span>Link to: {item.title}</span>
+								</Link>
+								{// Conditional render example
+								// Check to see if the background is orange, if so, display the message
+								item.background === "orange" ? (
+									<p style={{ color: item.initial }}>
+										Check store/store.js scroll to the actions to see the code
+									</p>
+								) : null}
+								<button
+									className="btn btn-success"
+									onClick={() => actions.changeColor(index, "orange")}>
+									Change Color
+								</button>
+							</li>
+						);
+					});
+				}}
+			</Context.Consumer>
+		</ul>
+		<br />
+		<Link to="/">
+			<button className="btn btn-primary">Back home</button>
+		</Link>
+	</div>
+);

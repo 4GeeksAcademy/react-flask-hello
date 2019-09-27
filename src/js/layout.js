22d0fd7f1d52215a5bbd9c1f5704a9b35450860a
jferragut <31458614+jferragut@users.noreply.github.com>
2019-09-27 13:33:38 -0400

    convert demo to function, add conditional render

--- src/js/layout.js
+++ src/js/layout.js
@@ -11,14 +11,14 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 //create your first component
-export const Layout = ()=> {
+export const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || '';
+	const basename = process.env.BASENAME || "";
 
 	return (
 		<div className="d-flex flex-column h-100">
-			<BrowserRouter basename={basename}>
+			<BrowserRouter>
 				<ScrollToTop>
 					<Navbar />
 					<Switch>

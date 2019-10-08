e174bdecb9f3c8eb9c2b521d8f8b8005d315d258
Alejandro Sanchez <aalejo@gmail.com>
2019-10-08 14:36:47 +0000

    updated home to  functional component

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,18 +2,14 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export class Home extends React.Component {
-	render() {
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
-			</div>
-		);
-	}
-}
+export const Home = () => (
+	<div className="text-center mt-5">
+		<h1>Hello Rigo!</h1>
+		<p>
+			<img src={rigoImage} />
+		</p>
+		<a href="#" className="btn btn-success">
+			If you see this green button, bootstrap is working
+		</a>
+	</div>
+);

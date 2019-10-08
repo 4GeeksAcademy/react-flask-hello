8c15e1a62d7b8ad4ab6b41655a6cb1497b9790f8
Alejandro Sanchez <aalejo@gmail.com>
2019-10-08 10:00:41 -0400

    Update appContext.js

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -26,6 +26,9 @@ const injectContext = PassedComponent => {
 			 * EDIT THIS!
 			 * This function is the equivalent to "window.onLoad", it only run once on the entire application lifetime
 			 * you should do your ajax requests or fetch api requests here
+			 *
+			 * state.loadSomeData(); <---- calling this function from the flux.js actions
+			 *
 			 **/
 		}, []);
 

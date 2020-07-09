466f84dc1eab8ef68f98f0ffd49e7ce21ae48e4d
Alejandro Sanchez <aalejo@gmail.com>
2020-07-09 14:29:00 -0400

    Update layout.js

--- src/js/layout.js
+++ src/js/layout.js
@@ -11,7 +11,7 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 //create your first component
-export const Layout = () => {
+const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";

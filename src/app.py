62a92eab7b743ba026944c566bcfb626d9fbcd64
Alejandro Sanchez <aalejo@gmail.com>
2020-09-11 18:07:53 +0000

    finished version 0.1

--- src/app.py
+++ src/app.py
@@ -56,5 +56,5 @@ def serve_any_other_file(path):
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -27,10 +27,8 @@ const injectContext = PassedComponent => {
 			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
 			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
 			 * store, instead use actions, like this:
-			 *
-			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
-			 *
 			 **/
+			state.actions.getMessage(); // <---- calling this function from the flux.js actions
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,

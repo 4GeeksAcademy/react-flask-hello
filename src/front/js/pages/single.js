d3563aa25f47e9866777568594e017065a3a8a9c
Alejandro Sanchez <aalejo@gmail.com>
2021-02-18 15:40:55 +0000

    fixed some bugs when going to production and using subdirectories

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
 
 export const Single = props => {
 	const { store, actions } = useContext(Context);
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[props.match.params.theid].title}</h1>
+	const params = useParams();
 
+    return (
+		<div className="jumbotron">
+			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
+            <img src={rigoImageUrl} />
 			<hr className="my-4" />
 
 			<Link to="/">

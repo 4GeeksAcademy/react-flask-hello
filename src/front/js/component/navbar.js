d90d5cfe80bc5f69b89512db79fe82e58957965f
Alejandro Sanchez <aalejo@gmail.com>
2021-09-17 19:11:09 +0000

    updated to bootstrap 5

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
+				</div>
 			</div>
 		</nav>
 	);

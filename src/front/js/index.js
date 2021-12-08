f07525e74d45f31f06dd0e4f86d6c5e72b460f9f
Alejandro Sanchez <aalejo@gmail.com>
2021-12-08 22:04:21 +0000

    trying to make it compatible with node 16

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -2,9 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
-import "bootstrap/dist/css/bootstrap.css";
-
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,41 +1,45 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const path = require('path');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const PrettierPlugin = require("prettier-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+// const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 const port = 3000;
-let publicUrl = `http://localhost:${port}`;
+let publicUrl = `ws://localhost:${port}/ws`;
 if(process.env.GITPOD_WORKSPACE_URL){
   const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
-  publicUrl = `${port}-${host}`;
+  publicUrl = `wss://${port}-${host}/ws`;
 }
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'cheap-module-source-map',
     devServer: {
-        contentBase:  './dist',
+        port,
         hot: true,
-        disableHostCheck: true,
+        allowedHosts: "all",
         historyApiFallback: true,
-        public: publicUrl
+        static: {
+          directory: path.resolve(__dirname, "dist"),
+        },
+        client: {
+          webSocketURL: publicUrl
+        },
     },
     plugins: [
-        new FriendlyErrorsWebpackPlugin(),
-        new ErrorOverlayPlugin(),
-        new PrettierPlugin({
-            parser: "babylon",
-            printWidth: 120,             // Specify the length of line that the printer will wrap on.
-            tabWidth: 4,                // Specify the number of spaces per indentation-level.
-            useTabs: true,              // Indent lines with tabs instead of spaces.
-            bracketSpacing: true,
-            extensions: [ ".js", ".jsx" ],
-            jsxBracketSameLine: true,
-            semi: true,                 // Print semicolons at the ends of statements.
-            encoding: 'utf-8'           // Which encoding scheme to use on files
-        }),
+        // new FriendlyErrorsWebpackPlugin(),
+        // new ErrorOverlayPlugin(),
+        // new PrettierPlugin({
+        //     parser: "babylon",
+        //     printWidth: 120,             // Specify the length of line that the printer will wrap on.
+        //     tabWidth: 4,                // Specify the number of spaces per indentation-level.
+        //     useTabs: true,              // Indent lines with tabs instead of spaces.
+        //     bracketSpacing: true,
+        //     extensions: [ ".js", ".jsx" ],
+        //     jsxBracketSameLine: true,
+        //     semi: true,                 // Print semicolons at the ends of statements.
+        //     encoding: 'utf-8'           // Which encoding scheme to use on files
+        // }),
         new webpack.HotModuleReplacementPlugin()
     ]
 });

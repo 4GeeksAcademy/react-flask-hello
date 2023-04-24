9bdbb5d3f1cede2ed0f15c08c5f204d622c4b86c
Alejandro Sanchez <aalejo@gmail.com>
2023-04-24 12:34:49 -0400

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,9 +6,16 @@ const common = require('./webpack.common.js');
 
 const port = 3000;
 let publicUrl = `ws://localhost:${port}/ws`;
+
+//only for github
 if(process.env.GITPOD_WORKSPACE_URL){
   const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
   publicUrl = `wss://${port}-${host}/ws`;
+}
+
+//only for codespaces
+if(process.env.CODESPACE_NAME){
+  publicUrl = `wss://${process.env.CODESPACE_NAME}-${port}.preview.app.github.dev/ws`;
 }
 
 module.exports = merge(common, {

3d0dbd4e24aeb1dccc0303aa5568137cc16818d5
Tozzigh <61799388+Tozzigh@users.noreply.github.com>
2023-05-04 08:58:56 +0200

    Update reset_migrations.bash

--- docs/assets/reset_migrations.bash
+++ docs/assets/reset_migrations.bash
@@ -1,7 +1,7 @@
 rm -R -f ./migrations &&
 pipenv run init &&
-psql -U gitpod -c 'DROP DATABASE example;' || true &&
-psql -U gitpod -c 'CREATE DATABASE example;' &&
-psql -U gitpod -c 'CREATE EXTENSION unaccent;' -d example &&
+psql -h localhost -U gitpod postgres -c 'DROP DATABASE example;' || true &&
+psql -h localhost -U gitpod postgres -c 'CREATE DATABASE example;' &&
+psql -h localhost -U gitpod example -c 'CREATE EXTENSION unaccent;' -d example &&
 pipenv run migrate &&
 pipenv run upgrade

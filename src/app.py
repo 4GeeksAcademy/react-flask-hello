6a48ec1985ddea971ba032fac85b02b0a0263aa7
Alejandro Sanchez <aalejo@gmail.com>
2023-06-19 12:12:58 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ from api.commands import setup_commands
 
 #from models import Person
 
-ENV = os.getenv("FLASK_ENV")
+ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False

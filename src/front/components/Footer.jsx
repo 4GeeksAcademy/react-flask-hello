6b119bde711a65c97f3e0e49bc923f4a8cec30f5
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2025-02-20 13:01:54 +0000

    feat: add BackendURL component and improve env variable handling
    - Added  component to handle missing environment variables.
    - Updated  to check for  and display  if not set.
    - Updated  to clarify that frontend env variables use  and must start with .
    - Modified , changing  to .

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/start-react-advanced-project">template documentation</a> <i class="fa-solid fa-file"></i> for help.
+			Check the <a target="_blank" href="https://4geeks.com/docs/start/start-react-advanced-project">template documentation</a> <i className="fa-solid fa-file"></i> for help.
 		</p>
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}

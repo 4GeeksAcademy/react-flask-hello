5268ef80025380e02fe203e571f38feafd82bce8
Rosinni Rodríguez <43686922+rosinni@users.noreply.github.com>
2025-02-25 14:47:54 +0000

    update python version

--- migrations/versions/0763d677d453_.py
+++ migrations/versions/0763d677d453_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: e16d9997f0f7
+Revision ID: 0763d677d453
 Revises: 
-Create Date: 2025-02-11 19:12:43.374246
+Create Date: 2025-02-25 14:47:16.337069
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e16d9997f0f7'
+revision = '0763d677d453'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('email', sa.String(length=120), nullable=False),
-    sa.Column('password', sa.String(length=80), nullable=False),
+    sa.Column('password', sa.String(), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email')

0657069ec457043083fd06521954598cf1a9bef7
Angel Ponte <angeld0606@gmail.com>
2025-03-31 15:00:21 +0000

    carpetas del SRC modificadas

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,4 +1,3 @@
-  
 import os
 from flask_admin import Admin
 from .models import db, User
@@ -10,8 +9,5 @@ def setup_admin(app):
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
     
-    # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
 
-    # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(User, db.session))
--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -3,13 +3,15 @@ import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
 
-// Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
+
 export const Layout = () => {
     return (
+
         <ScrollToTop>
             <Navbar />
                 <Outlet />
             <Footer />
         </ScrollToTop>
+        
     )
 }

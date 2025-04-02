881dc7b065e1bc44a8cfbd68b233ea7b3226fd2e
Yenesey2001 <yeneseyclase@gmail.com>
2025-04-02 10:08:14 +0000

    model actualizado

--- src/api/models/models.py
+++ src/api/models/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import Integer
+from sqlalchemy import Date, Float, String, ForeignKey
 
 
 db = SQLAlchemy()
@@ -38,7 +39,58 @@ class User(db.Model):
         }
 
 
-## MODELO DE PARCELA (Con serialize)
-## MODELO DE IMAGEN (Con serialize)
-## MODELO DE INFORME (Con serialize)
-## MODELO DE PRESUPUESTO (Con serialize)
+# MODELO DE PARCELA (Con serialize)
+class Field(db.Model):
+    __tablename__ = "fields"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100))
+
+    area: Mapped[float] = mapped_column(Float)
+    crop: Mapped[str] = mapped_column(String(100))
+    sowing_date: Mapped[Date] = mapped_column(Date)
+
+    street: Mapped[str] = mapped_column(String(100))
+    number: Mapped[str] = mapped_column(String(10))
+    postal_code: Mapped[str] = mapped_column(String(10))
+    city: Mapped[str] = mapped_column(String(100))
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    def serialize_field(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "area": self.area,
+            "crop": self.crop,
+            "sowing_date": self.sowing_date.isoformat() if self.sowing_date else None,
+            "street": self.street,
+            "number": self.number,
+            "postal_code": self.postal_code,
+            "city": self.city,
+            "user_id": self.user_id
+        }
+
+# MODELO DE IMAGEN (Con serialize)
+
+
+class Image(db.Model):
+    __tablename__ = "images"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    filename: Mapped[str] = mapped_column(String(255))
+    url: Mapped[str] = mapped_column(String(255))
+    field_id: Mapped[int] = mapped_column(ForeignKey("fields.id"))
+    upload_date: Mapped[Date] = mapped_column(Date)
+
+    def serialize_img(self):
+        return {
+            "filename": self.filename,
+            "url": self.url,
+            "field_id": self.field_id,
+            "upload_date": self.upload_date
+        }
+
+
+# MODELO DE INFORME (Con serialize)
+# MODELO DE PRESUPUESTO (Con serialize)

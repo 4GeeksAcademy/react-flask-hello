24b121ec05f6dcebeb3957567448d5b529206612
Joan Gallego Frances <123939267+joangallego@users.noreply.github.com>
2025-04-02 11:09:04 +0000

    vista de login

---
+++ src/front/hooks/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./front/pages/Login";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+);

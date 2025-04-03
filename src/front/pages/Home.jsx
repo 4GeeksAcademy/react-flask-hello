0447c5ebc374f0538e4cf4bfed40ef873f8d4d12
Joan Gallego Frances <123939267+joangallego@users.noreply.github.com>
2025-04-03 12:21:20 +0000

    Componente navbar

---
+++ src/front/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar"; 
+
+const home = () => {
+  return (
+    <div style={{ backgroundColor: "#CAD2C5", minHeight: "100vh" }}>
+        <Navbar />
+    </div>
+  );
+};
+
+export default home;

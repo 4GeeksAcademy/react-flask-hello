92c2f9df35fba3e8394a9a6bdfbe35cde744fb7f
Joan Gallego Frances <123939267+joangallego@users.noreply.github.com>
2025-04-03 12:21:20 +0000

    Componente navbar

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-
-
+import Home from "./pages/Home";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
     );
 };

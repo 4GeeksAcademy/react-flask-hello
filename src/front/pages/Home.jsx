4af4fb3aabc5ba04287d590c06496b93b2df898c
Joan Gallego Frances <123939267+joangallego@users.noreply.github.com>
2025-04-04 09:50:47 +0000

    footer creado

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar"; 
+import Footer from "../components/Footer"
 
 const home = () => {
   return (
+    <>
     <div style={{ backgroundColor: "#CAD2C5", minHeight: "100vh" }}>
         <Navbar />
     </div>
+
+    <div style={{ backgroundColor: "#CAD2C5"}}>
+      <Footer />
+    </div>
+    </>
   );
 };
 

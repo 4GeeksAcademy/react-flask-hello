db2f19fb6c4c62aa5a8b1509027089f793db2d0b
javigvr <jsanchezgav@gmail.com>
2025-04-12 10:44:25 +0000

    CSS dash admin solucionado

--- src/front/pages/Dash_admin/Dash_admin.jsx
+++ src/front/pages/Dash_admin/Dash_admin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import "./Dash_admin.css"; 
 const DashboardAdmin = () => {
   const [users, setUsers] = useState([]);
 

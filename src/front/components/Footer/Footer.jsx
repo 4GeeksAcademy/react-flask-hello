1eb0793adf888a26c36dc9596ea5218c23c91dc2
Angel Ponte <angeld0606@gmail.com>
2025-04-14 12:49:01 +0000

    css del footer modificado

--- src/front/components/Footer/Footer.jsx
+++ src/front/components/Footer/Footer.jsx
@@ -5,40 +5,19 @@ export const Footer = () => {
 	return (
 		<footer className="footer-container">
 			<div className="footer-content">
-				<div className="footer-nav">
-					<div className="footer-section">
-						<button className="footer-btn">
-							<i className="bi bi-info-circle"></i>
-							<span>Sobre nosotros</span>
-						</button>
-					</div>
-					<div className="footer-section">
-						<button className="footer-btn">
-							<i className="bi bi-gear"></i>
-							<span>Servicios</span>
-						</button>
-					</div>
-					<div className="footer-section">
-						<button className="footer-btn">
-							<i className="bi bi-envelope"></i>
-							<span>Contacto</span>
-						</button>
-					</div>
-				</div>
-				<div className="footer-divider"></div>
 				<div className="footer-bottom">
 					<p className="copyright">
-						&copy; Flow {new Date().getFullYear()}. Todos los derechos reservados.
+						&copy; Flow {new Date().getFullYear()}. All rights reserved.
 					</p>
 					<div className="social-links">
 						<a href="#" className="social-link">
-							<i className="bi bi-linkedin"></i>
+							<i class="fa-brands fa-linkedin"></i>
 						</a>
 						<a href="#" className="social-link">
-							<i className="bi bi-twitter-x"></i>
+							<i class="fa-brands fa-square-x-twitter"></i>
 						</a>
 						<a href="#" className="social-link">
-							<i className="bi bi-facebook"></i>
+							<i class="fa-brands fa-square-facebook"></i>
 						</a>
 					</div>
 				</div>

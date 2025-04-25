cb01abca9bf07edee9dea8e5e86e46f21300962a
Lovnyth Valcin <lovnyth@gmail.com>
2025-04-25 23:46:52 +0000

    created models table for signup

--- src/api/models.py
+++ src/api/models.py
@@ -8,12 +8,15 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False,unique=False)
+    age: Mapped[int] = mapped_column(nullable=False, unique= False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "age": self.age
             # do not serialize the password, its a security breach
         }

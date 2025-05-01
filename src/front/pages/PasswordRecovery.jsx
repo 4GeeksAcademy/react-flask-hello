2ed895f6533fa379a70867f2ae7d8c38be54851e
Lovnyth Valcin <lovnyth@gmail.com>
2025-05-01 00:13:07 +0000

    password recovery component created

---
+++ src/front/pages/PasswordRecovery.jsx
@@ -0,0 +1,20 @@
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import React, { useEffect } from 'react'
+import { Link, useParams } from "react-router-dom";
+
+export const PasswordRecovery = () => {
+
+
+}
+
+// useEffect(()=> {
+// },[]);
+
+return (
+    <div >
+        PW Recovery Page
+
+    </div>
+
+)
+};

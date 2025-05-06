b55b78032b478d01d6b78ecfbb3d0693d4348e0c
XxDrancerxX <idmdiazmora98@gmail.com>
2025-05-06 01:06:18 +0000

    sig-in-started

--- src/api/models.py
+++ src/api/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]= mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)

d0dc8bd8adcaa4bcec5b65ffb15e730eb9e12875
XxDrancerxX <idmdiazmora98@gmail.com>
2025-05-09 15:37:48 +0000

    password-reset-done

--- src/front/pages/ResetPassword.jsx
+++ src/front/pages/ResetPassword.jsx
@@ -19,61 +19,7 @@ export const ResetPassword = () => {
     const [loading, setLoading] = useState(false); //==> We create a loading state to show a loading spinner when the user clicks the submit button and the API is being called.
 
 
-    // 3) We create a function to handle the form submission in the form.
-    // const handleSubmit = async (e) => {//==> asynchronous function that will be called when the user clicks the submit button
-    //     e.preventDefault(); //==> Prevent the default behavior of the form submission when the user clicks the submit button(refreshing the page)
-    //     setError(null); //==> Reset the error message to null when the user clicks the submit button
-    //     if (newPassword !== confirmPassword) { //==> Check if the new password and confirm password are not equal
-    //         setError("Passwords do not match")
-    //         return; //==> early exit the function stopping the execution of the code below. so the coode don't call the API on the backend.
-    //     }
-
-    //     setLoading(true); //==> Set the loading state to true when the user clicks the submit button
-
-    //     //==> We create a payload object to send to the backend with the token and the new password
-    //     // const payload = {
-    //     //     token: token,
-    //     //     new_password: newPassword
-    //     // // }
-    //     // console.log("======>>>>!!Payload going to server:>>>", payload);
-    //     console.log("=====>>>>!!!PAYLOAD:>>>>", JSON.stringify({ token, new_password: newPassword }));
-    //     // 4) Call the API to reset the password
-    //     console.log("🍃 VITE_BACKEND_URL is:", import.meta.env.VITE_BACKEND_URL);
-
-    //     try {
-    //         const callResetPassword = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/reset-password`,
-    //             {
-    //                 method: "PUT", //==> We use the PUT method to update the password.
-    //                 headers: {
-    //                     "Content-Type": "application/json"
-    //                 },
-    //                 body: JSON.stringify({ token, new_password: newPassword }) //==> We send the payload object to the backend
-    //             })
-    //         console.log("BACKEND:", import.meta.env.VITE_BACKEND_URL);
-
-    //         const body = await callResetPassword.json().catch(()=>{})
-    //         if (!callResetPassword.ok) {
-    //             const errMsg = body.error || "Something went wrong. Please try again.";
-    //             setError(errMsg)
-    //             return; //==> early exit the function stopping the execution of the code below.
-    //         }
-
-    //         console.log("==>>>Server said:", body);
-
-    //         alert("Password successfully changed")
-    //         navigate("/signin", { replace: true }) //==> If the API call is successful, we navigate to the sign in page // It swaps the history entry so users can’t hit “Back” and re-submit the form.
-    //     }
-
-    //     // catch (err) {
-    //     //     setError(err.message || "Unknown Error") //==> If the API call fails, we set the error message to the error message returned by the API or to "Unknown Error" if no error message is returned.
-    //     //      console.log(err.message);
-
-    //     // }
-    //     // finally { //==> This block of code will always be executed after the try and catch blocks, regardless of whether an error occurred or not.
-    //     //     setLoading(false) //==> Set the loading state to false when the API call is finished
-    //     // }
-
-    // }
+    // 3) We create a function to handle the form submission in the form.   
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError(null);

996ae776cefd4f8698ba7d37342ee22a72ac5a1c
Alberto Delicado Delicado <45593059+exalbertod@users.noreply.github.com>
2025-05-16 06:29:59 +0000

    finalisima

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
-    host: '0.0.0.0', // NECESARIO para GitHub Codespaces
+    host: '0.0.0.0',
     port: 3000
   },
   build: {

b18a3428deff80304e1c6265421aa65eb8464a12
Jonas <jonas.backline@gmail.com>
2025-05-24 14:24:02 +0000

    funcion agregar al  carrito

---
+++ src/front/data/products.js
@@ -0,0 +1,28 @@
+// src/front/data/products.js
+
+export const products = [
+  {
+    id: 1,
+    name: "Camiseta Blanca",
+    description: "Camiseta de algodón 100%",
+    price: 19.99,
+    gender: "Unisex",
+    size: "M"
+  },
+  {
+    id: 2,
+    name: "Pantalón Vaquero",
+    description: "Pantalón azul clásico",
+    price: 39.99,
+    gender: "Hombre",
+    size: "L"
+  },
+  {
+    id: 3,
+    name: "Sudadera Negra",
+    description: "Sudadera con capucha",
+    price: 29.99,
+    gender: "Mujer",
+    size: "S"
+  }
+];

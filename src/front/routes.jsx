72df2119752dace4ecd649d73a88b625171b5282
GuidoCanso <guidocansobre@gmail.com>
2025-05-24 14:46:44 +0000

    Rutas arregladas

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,25 +1,18 @@
-// Import necessary components and functions from react-router-dom.
 
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-} from "react-router-dom";
-import { Layout } from "./pages/Layout";
-import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Feed from "./pages/Feed"
+
+
 
 export const router = createBrowserRouter(
-    createRoutesFromElements(
-    // CreateRoutesFromElements function allows you to build route elements declaratively.
-    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
-    // Root, on the contrary, create a sister Route, if you have doubts, try it!
-    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
-    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
+  createRoutesFromElements(
 
-      // Root Route: All navigation will start from here.
-      <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -28,5 +21,5 @@ export const router = createBrowserRouter(
         <Route path="/feed" element={<Feed />} />
       </Routes>
     </Router>
-    )
+  )
 );

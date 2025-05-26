e5b3986caa83037b32411efb78dabfb1b9ea8352
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-05-26 19:30:47 +0000

    profile en proceso

---
+++ src/front/components/Profile.jsx
@@ -0,0 +1,25 @@
+import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+
+export const Profile = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6 text-center">
+                    <h2> Bienvenido José Alejandro</h2>
+                    <img src={rigoImageUrl} alt="" />
+                </div>
+                <div className="col-6">
+                    <h5 className="fw-bold">Información básica</h5>
+                    <ul className="list-group list-group-flush text-center">
+                        <li className="list-group-item"><div className="fw-bold">Apellidos:</div>Guzmán Mendoza</li>
+                        <li className="list-group-item"><div className="fw-bold">Nombres:</div>José Alejandro</li>
+                        <li className="list-group-item"><div className="fw-bold">ID:</div>a987654321</li>
+                        <li className="list-group-item"><div className="fw-bold">Correo:</div>alejandroguzman@correo.com</li>
+                        <li className="list-group-item"><div className="fw-bold">Teléfono:</div>987654321</li>
+                        <li className="list-group-item"><div className="fw-bold">Año:</div>Primero</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};

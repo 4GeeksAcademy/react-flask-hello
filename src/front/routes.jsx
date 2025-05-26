19ec23974544c87253797afb99c85526ece81389
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-05-26 22:43:23 +0000

    footer actualizado

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -9,7 +9,7 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
-import { Profile } from "./components/Profile";
+import { Profile } from "./pages/Profile";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(

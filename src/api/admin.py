8e9cb0389830296abcb95cf2cc127eb64361c929
Juan Felipe Rios Torres <141080920+JuanFelipeR@users.noreply.github.com>
2025-05-27 21:10:20 +0000

    Modelo actualizado

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, AcademicYear, Student, Teacher, Course, Schedule, Enrollment, Attendance, Grade, Payment
+from .models import db, User, Student, Teacher, Course, Schedule, Enrollment, Attendance, Grade, Payment
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,7 +12,6 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(AcademicYear, db.session))
     admin.add_view(ModelView(Student, db.session))
     admin.add_view(ModelView(Teacher, db.session))
     admin.add_view(ModelView(Enrollment, db.session))

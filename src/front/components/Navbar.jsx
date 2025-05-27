0725a97236d2af757f2391192566f3111a86678c
Marco Pastene <mpastene1996@outlook.com>
2025-05-27 21:11:45 +0000

    integracion

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -22,17 +22,13 @@ export const Navbar = () => {
 						{/* Columna para los botones */}
 						<div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-end gap-2">
 							<Link to="/about">
-<<<<<<< HEAD
 								<button className="btn btn-outline-black">About Us</button>
-=======
 								<button className="btn btn-outline-dark px-3">About Us</button>
->>>>>>> 43641c9f75967f93afc7c82f44b3da521168ef7e
 							</Link>
 						</div>
 					</div>
 				</div>
 			</nav>
-<<<<<<< HEAD
 
 			<div className="container text-center mb-5 px-3 mt-5">
 				<h1 style={{
@@ -47,8 +43,6 @@ export const Navbar = () => {
 					<h2><strong>Tu mejor compañero para realizar los mejores eventos</strong></h2>
 				</p>
 			</div>
-=======
->>>>>>> 43641c9f75967f93afc7c82f44b3da521168ef7e
 		</header>
 	);
 };

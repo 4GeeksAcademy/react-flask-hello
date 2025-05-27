48c81e760235ac0834b3c28bf9b0fc25ba37c626
ff.mendieta <82422221+INS4NELY@users.noreply.github.com>
2025-05-27 22:11:32 +0000

    Add Login User

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,52 +1,87 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [user, setUser] = useState('alumno')
+	const [msg, setMsg] = useState('')
+	const navigate = useNavigate();
 
-	const loadMessage = async () => {
+	const handleOnSubmit = async (e) => {
+		e.preventDefault();
+		if (!email || !password) {
+			setMsg("*Completa los campos correctamente.");
+			return;
+		} else setMsg('')
+		const body = JSON.stringify({ email, password })
+
 		try {
-			const backendUrl = import.meta.env.VITE_BACKEND_URL
+			const response = await fetch(`https://probable-space-enigma-6pqgwjg9vxvf4ww7-3001.app.github.dev/${user}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body,
+			})
 
-			if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
-
-			const response = await fetch(backendUrl + "/api/hello")
 			const data = await response.json()
 
-			if (response.ok) dispatch({ type: "set_hello", payload: data.message })
-
-			return data
+			if (response.ok) {
+				localStorage.setItem("token", data.access_token);
+				dispatch({ type: "SET_USER", payload: { email: email } });
+				dispatch({ type: "SET_TOKEN", payload: data.access_token });
+				navigate(`/${user}/dashboard`);
+			}
 
 		} catch (error) {
-			if (error.message) throw new Error(
-				`Could not fetch the message from the backend.
-				Please check if the backend is running and the backend port is public.`
-			);
-		}
+			console.log(error);
+			setMsg('*Email o contraseña son incorrectos')
 
+		}
 	}
 
-	useEffect(() => {
-		loadMessage()
-	}, [])
-
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
+		<div >
+			<div className="d-flex position-absolute top-50 start-50 translate-middle gap-5 align-items-center border border-1 border-secondary rounded-3"  >
+				<div>
+					<img src="src/front/assets/img/loginUser.jpg" alt="" className="imgLogin rounded-start" />
+				</div>
+				<form className="AdminLoginWidth me-5" onSubmit={handleOnSubmit}>
+					<h1 className="text-center mb-5">Login</h1>
+					<div className="d-flex flex-column gap-3 mb-4">
+						<div>
+							<label className="form-label">Email</label>
+							<input name="email" type="email" className="form-control" onChange={e => setEmail(e.target.value)} required />
+						</div>
+						<div>
+							<label className="form-label">Password</label>
+							<input name="password" type="password" className="form-control" onChange={e => setPassword(e.target.value)} required />
+						</div>
+
+					</div>
+					<div className="d-flex justify-content-between">
+						<div className="form-check">
+							<input className="form-check-input " type="radio" name="radioDefault" id="radioDefault1" onChange={() => setUser('alumno')} checked={user === 'alumno'} />
+							<label className="form-check-label" htmlFor="radioDefault1">
+								Alumno
+							</label>
+						</div>
+						<div className="form-check">
+							<input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" onChange={() => setUser('profesor')} checked={user === 'profesor'} />
+							<label className="form-check-label" htmlFor="radioDefault2">
+								Profesor
+							</label>
+						</div>
+					</div>
+					<p className="text-danger mt-2">{msg}</p>
+					<button type="submit" className="btn btn-outline-dark w-100">Login</button>
+				</form>
 			</div>
 		</div>
-	);
+	)
 }; 

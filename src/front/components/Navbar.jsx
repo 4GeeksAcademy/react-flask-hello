95c3ad5fec882d5ed9c2ff90565969c3dd8274cc
DaivdVivar <davidvivarmartinez@gmail.com>
2025-05-28 09:05:51 +0000

    prueba

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -7,6 +7,7 @@ export const Navbar = () => {
   <div class="container-fluid">
     <a class="navbar-brand"><img src="/logoCrema.png" className="logo-navbar" alt="Logo" /></a>
     <form class="d-flex" role="search">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
       <button class="btn btn-outline-success" type="submit">Search</button>
     </form>

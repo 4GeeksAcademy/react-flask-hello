9ef644afaa5c86691d3d12c60b4437a45ad25d43
DaivdVivar <davidvivarmartinez@gmail.com>
2025-05-28 11:04:43 +0000

    PRuebita lerole

--- src/front/pages/AboutUs.jsx
+++ src/front/pages/AboutUs.jsx
@@ -1,38 +1,31 @@
 import React from "react";
+import ReactPlayer from 'react-player'
 import { Navbar } from "../components/Navbar";
 import "../../styles/aboutus.css";
 
 const AboutUs = () => {
-  const selectedTrainer = {
-    image: "https://randomuser.me/api/portraits/men/75.jpg",
-    name: "Pepe",
-    datos: "Estoy especializado en Crossfit.",
-    titulos: "Ciencias del deporte",
-    selected: true,
-  };
+    const selectedTrainer = {
+        image: "https://randomuser.me/api/portraits/men/75.jpg",
+        name: "Pepe",
+        datos: "Estoy especializado en Crossfit.",
+        titulos: "Ciencias del deporte",
+        selected: true,
+    };
 
-  return (
-    <>
-      <Navbar />
-      <header>
-        <div className="row">
-          <div className="col-md-12">
-            <h1>EJJE</h1>
-
-            <div className="video-wrapper">
-              <iframe
-                src="https://www.youtube.com/embed/ScMzIvxBSi4?autoplay=1&mute=1&controls=0&loop=1&playlist=ScMzIvxBSi4"
-                title="YouTube video"
-                allow="autoplay; encrypted-media"
-                allowFullScreen
-                frameBorder="0"
-              ></iframe>
-            </div>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <header>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="video-wrapper">
+                            <ReactPlayer url='https://www.youtube.com/watch?v=4-zjQvTDnbw' playing='true' volume='0' />
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default AboutUs;

be62333dc23dafd5374b8cf818d4fa7e87921f86
MafeGalvis7 <mariafernandagalvis33@hotmail.com>
2025-05-28 08:41:09 +0000

    mainpage

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import VideoPlayer from "../components/VideoPlayer.jsx";
+import Carousel from "../components/Carousel.jsx";
 
 export const Home = () => {
 
@@ -34,6 +36,8 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
+			<VideoPlayer/> 
+			<Carousel/>
 			<h1 className="display-4">Hello Rigo!!</h1>
 			<p className="lead">
 				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />

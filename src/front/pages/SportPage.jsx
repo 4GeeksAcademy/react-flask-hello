627996eb16285422070fddb6a58ef6b1b11b51cd
Cere <c_cere2909@hotmail.com>
2025-05-30 11:25:21 +0000

    terminado

--- src/front/pages/SportPage.jsx
+++ src/front/pages/SportPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
+import CardPlan from "../components/CardPlan";
 
 // index.css
 import "../../styles/sport.css";
 
 const SportPage = () => {
+
+    const planes = [
+        { tittle: "Plan Pérdida de Peso", img: "/perdidaPeso.webp", parrafo: "Planes bajos en calorías con todos los deportes esenciales." },
+        { tittle: "Plan Ganancia Muscular", img: "/masa_muscular.jpg", parrafo: "Planificación para el crecimiento muscular." },
+        { tittle: "Plan Salud General", img: "/ejercicios_balanceados.jpg", parrafo: "Ejercicios balanceados para sentirte bien cada día." }
+    ]
+ 
     return (
         <div className="sport-page container mt-5 ">
             <section className="sport-header text-center py-5">
@@ -14,47 +22,19 @@ const SportPage = () => {
                 </p>
             </section>
 
-            {/* Planes de deporte */}
             <section className="planes my-5">
-                <h2 className="text-center subtittle mb-4 ">Nuestros Planes</h2>
+                <h2 className="text-center subtittle mb-4">Nuestros Planes</h2>
                 <div className="row">
-                    <div className="col-md-4">
-                        <div className="card h-100 shadow">
-                            <div className="card-body sport1">
-                                <h5 className="card-title mb-4">Plan Pérdida de Peso</h5>
-                                <img src="/perdidaPeso.webp" className="card-img-top" alt="Plan pérdida de peso" />
-                                <p className="card-text m-3">
-                                    Planes bajos en calorías con todos los deportes esenciales.
-                                </p>
+                    {planes.map((p) => {
+                        return (
+                            <div className="col-md-4">
+                                <CardPlan tittle={p.tittle} img={p.img} parrafo={p.parrafo}></CardPlan>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card h-100 shadow">
-                            <div className="card-body sport1">
-                                <h5 className="card-title mb-4">Plan Ganancia Muscular</h5>
-                                <img src="/masa_muscular.jpg" className="card-img-top" alt="Ganancia Muscular" />
-                                <p className="card-text m-3">
-                                    Planificación para el crecimiento muscular.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card h-100 shadow">
-                            <div className="card-body sport1">
-                                <h5 className="card-title mb-4">Plan Salud General</h5>
-                                <img src="/ejercicios_balanceados.jpg" className="card-img-top" alt="Salud General" />
-                                <p className="card-text m-3">
-                                    Ejercicios balanceados para sentirte bien cada día.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    })}
                 </div>
             </section>
-
-            {/* Beneficios */}
+ 
             <section className="beneficios my-5">
                 <h2 className="text-center subtittle mb-4">¿Por qué elegirnos?</h2>
                 <ul className="list-group list-group-flush caja-bot">

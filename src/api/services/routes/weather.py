b40e7dd754289ccd006bd42717abe0ca20437533
Daniel Landa <landadlh603@gmail.com>
2025-06-02 20:35:27 +0200

    Refactor el servicio de clima para simplificar la función de obtención de información meteorológica

--- src/api/services/routes/weather.py
+++ src/api/services/routes/weather.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from src.api.services.weather_services import get_weather_info
+from src.api.services.weather_services import get_weather
 
 weather_bp = Blueprint('weather_bp', __name__)
 
@@ -7,4 +7,4 @@ weather_bp = Blueprint('weather_bp', __name__)
 # Ruta para obtener el clima
 @weather_bp.route('/weather', methods=['GET'])
 def weather():
-    return get_weather_info()
+    return get_weather()

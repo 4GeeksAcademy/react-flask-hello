f5c5bbef81858bf821d0e76f455fd033ad64a07c
Cere <c_cere2909@hotmail.com>
2025-06-02 12:04:23 +0000

    pendiente

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -7,11 +7,11 @@ const Login = () => {
             <div class="container" id="container">
                 <div class="form-container">
                     <form>
-                        <h2>Crear Cuenta</h2>
-                        <span>Usa tu email para registrarte</span>
+                        <h2>Iniciar sesion</h2>
+                        <span>Usa tu email para iniciar sesion</span>
                         <input type="email" placeholder="Correo" required />
                         <input type="password" placeholder="Contraseña" required />
-                        <button>Registrarse</button>
+                        <button className="button_login">Login</button>
                     </form>
                 </div>
             </div>

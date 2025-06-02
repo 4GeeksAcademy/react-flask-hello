41fdd1400baca9ed7ef23146cfed8806696d8d60
Robertjsr <Eryero02@gmail.com>
2025-06-02 18:29:27 +0000

    footer con iconos

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,16 +1,34 @@
-
 import { Link } from "react-router-dom";
+import "../pages/Footer.css";
 
 export const Footer = () => {
     return (
         <footer className="bg-dark py-3 mt-auto w-100">
-            <div className="container d-flex justify-content-center gap-4">
+            <div className="container justify-content-end d-flex gap-4">
                 <Link to="/contacto" className="text-white text-decoration-none">
                     Contacto
                 </Link>
                 <Link to="/sobre-nosotros" className="text-white text-decoration-none">
                     Sobre nosotros
                 </Link>
+                <button
+                    href="https://instagram.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-outline-light btn-sm icon"
+                    aria-label="Instagram"
+                >
+                    <i className="bi bi-instagram"></i>
+                </button>
+                <button
+                    href="https://github.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-outline-light btn-sm icon2"
+                    aria-label="GitHub"
+                >
+                    <i className="bi bi-github"></i>
+                </button>
             </div>
         </footer>
     );

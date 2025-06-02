a8b7f49cfc79497f579437c1049dba36a3e9256f
Daniel Landa <landadlh603@gmail.com>
2025-06-02 21:50:03 +0200

    Corrige la inicialización del puerto en app.py para establecer un valor predeterminado

--- src/app.py
+++ src/app.py
@@ -112,7 +112,8 @@ def serve_any_other_file(path):
     return response
 
 
+port = int(os.environ.get('PORT', 4000 ))
+
 # Iniciar servidor
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT' ))
     app.run(host='0.0.0.0', port=port, debug=True)

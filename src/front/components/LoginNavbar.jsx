d6d3ad3543587340223278f9f05aa73b2d87c1ba
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-06-03 21:05:59 +0000

    mejora interfaces

--- src/front/components/LoginNavbar.jsx
+++ src/front/components/LoginNavbar.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 
 export const LoginNavbar = () => {
     return (
         <nav className="navbar navbar-light bg-light d-flex justify-content-between align-items-center gap-3 px-5 ">
             <div className='d-flex align-items-center'>
-                <Link to="/" className=" navbar-brand fs-2 h1">
+                <Link to="/" className="navbar-brand fs-2 h1">
                     αlpha
                 </Link>
             </div>
             <div className='d-flex align-items-center'>
-                <Link to='/signup' className=' signup-login-button text-decoration-none fw-semibold btn btn-outline-dark rounded-5'>Signup</Link>
+                <Link to='/signup' className=' signup-login-button text-decoration-none fw-semibold btn btn-dark rounded-5'>
+                    Signup
+                </Link>
             </div>
         </nav>
     )
--- src/front/components/SignupNavbar.jsx
+++ src/front/components/SignupNavbar.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 
 export const SignupNavbar = () => {
     return (
-        <nav className="navbar navbar-light bg-light d-flex justify-content-between align-items-center gap-3 py-3 px-5">
-
-            <div className='ms-3'>
-                <Link to="/" className=" navbar-brand fs-2 h1">
+        <nav className="navbar navbar-light bg-light d-flex justify-content-between align-items-center gap-3 px-5">
+            <div className='d-flex align-items-center'>
+                <Link to="/" className="navbar-brand fs-2 h1">
                     αlpha
                 </Link>
             </div>
-            <div className='me-5 '>
-                <Link to='/' className=' signup-login-button text-decoration-none fw-semibold btn btn-outline-dark rounded-5 '>Login</Link>
+            <div className='d-flex align-items-center'>
+                <Link to='/' className=' signup-login-button text-decoration-none fw-semibold btn btn-dark rounded-5 '>
+                    Login
+                </Link>
             </div>
-
         </nav>
     )
 }

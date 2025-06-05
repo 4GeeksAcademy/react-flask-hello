f149e01168d8a8a4721de16695b5d19edcb792c5
Cere <c_cere2909@hotmail.com>
2025-06-05 09:36:24 +0000

    modificiado

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "../../styles/login.css";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // navigate("/"); queda pendiente
+    };
+
 
     return (
         <div className="container_login">
             <div className="form-container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h2 className="mt-5">Iniciar sesión</h2>
                     <span>Usa tu email para iniciar sesión</span>
                     <input type="email" placeholder="Correo" required />
@@ -18,13 +27,15 @@ const Login = () => {
                                 <input type="checkbox" className="me-4" />
                                 Recuérdame
                             </label>
-                            <a href="#" className="forgot-password me-4">¿Olvidaste tu contraseña?</a>
+                            <Link to="/register" className="forgot-password me-3">
+                                ¿No tienes cuenta? Regístrate
+                            </Link>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Login;

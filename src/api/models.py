99383cc0c27345d9b3051abc6b18e30caf2b9c6b
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-06-06 09:30:04 +0000

    Agregar columna 'name' como nullable en el modelo User y crear archivos de migración para la configuración de la base de datos.

--- src/api/models.py
+++ src/api/models.py
@@ -13,6 +13,7 @@ class User(db.Model):
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(120) )
 
     created_events = db.relationship('Event', backref='creator', lazy=True)
 

e82bb1cde8796b77774e7467a007e3b2d92b4d3e
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-06-06 13:24:54 +0000

    Habilitar el uso del componente PrivateRoute en la ruta de perfil

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -16,7 +16,7 @@ export const router = createBrowserRouter(
       <Route
         path="/profile"
         element={
-          // <PrivateRoute>
+          <PrivateRoute>
             <Profile />
           </PrivateRoute>
         }

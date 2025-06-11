113fe41cac638eca01225a8bea8477c4b02c6209
Cere <c_cere2909@hotmail.com>
2025-06-11 11:05:55 +0000

    terminado

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -29,17 +29,12 @@ const Login = () => {
         <div className="container_login">
             <div className="form-container">
                 <form onSubmit={handleSubmit}>
-                    <h2 className="mt-5">Iniciar sesión</h2>
-                    <span>Usa tu email para iniciar sesión</span>
+                    <h1 className="mt-5">Iniciar sesión</h1>
                     <input type="email" name="email" onChange={handleChange} placeholder="Correo" required />
                     <input type="password" name="password" onChange={handleChange} placeholder="Contraseña" required />
                     <button className="button_login">Login</button>
                     <div className="form-footer">
                         <div className="options-row">
-                            <label className="remember-label">
-                                <input type="checkbox" className="me-4" />
-                                Recuérdame
-                            </label>
                             <Link to="/register" className="forgot-password me-3">
                                 ¿No tienes cuenta? Regístrate
                             </Link>
--- src/front/pages/Register.jsx
+++ src/front/pages/Register.jsx
@@ -26,25 +26,11 @@ const Register = () => {
         <div className="container_register">
             <div className="form-container">
                 <form onSubmit={handleSubmit}>
-                    <h2 className="mt-5">Register</h2>
+                    <h1 className="mt-5">Register</h1>
                     <input type="email" name="email" onChange={handleChange} placeholder="Correo" required />
                     <input type="password" name="password" onChange={handleChange} placeholder="Password" required />
                     <input type="submit" className="button_login" />
                 </form>
-                <div className="options-row">
-                    <label className="remember-label">
-                        <input type="checkbox" className="me-4"/>
-                        Recuérdame
-                    </label>
-                </div>
-                <div className="form-footer">
-                    <div className="options-row1">
-                        <label className="remember-label">
-                            <input type="checkbox" className="checkbox1 me-4" />
-                            Soy mayor de 16 años y acepto los &nbsp;<a href="#">Términos y condiciones</a>
-                        </label>
-                    </div>
-                </div>
             </div>
         </div>
     )

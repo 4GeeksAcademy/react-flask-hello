c6017b7c2077c199e203a270049ca5b13d3f51a9
Cere <c_cere2909@hotmail.com>
2025-06-12 10:08:54 +0000

    modificado

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,10 +7,10 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
-import  NutricionPage  from "./pages/NutricionPage";
-import  SportPage  from "./pages/SportPage";
-import  Entrenadores  from "./pages/Entrenadores";
-import  AboutUs  from "./pages/AboutUs";
+import NutricionPage from "./pages/NutricionPage";
+import SportPage from "./pages/SportPage";
+import Entrenadores from "./pages/Entrenadores";
+import AboutUs from "./pages/AboutUs";
 import Eventos from "./pages/Eventos";
 import Tarifas from "./pages/Tarifas";
 import User from "./pages/User";
@@ -18,10 +18,10 @@ import ProfesoresPage from "./pages/ProfesoresPage";
 import Register from "./pages/Register";
 import { Checkout } from "./pages/checkout";
 import Login from "./pages/Login";
-import  { ReturnPage }  from "./pages/returnPage";
+import { ReturnPage } from "./pages/returnPage";
 import PaymentReturn from "./components/paymentReturn";
-import  RegisterPage  from "./pages/RegisterPage";
-import  LoginPage  from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 import { PrivatePage } from "./pages/PrivatePage";
 
 
@@ -33,7 +33,7 @@ export const router = createBrowserRouter(
       <Route path="/single/:theId" element={<Single />} />
       <Route path="/demo" element={<Demo />} />
       <Route path="/nutricion" element={<NutricionPage />} />
-      <Route path="/sport" element={<SportPage />} /> 
+      <Route path="/sport" element={<SportPage />} />
       <Route path="/entrenadores" element={<Entrenadores />} />
       <Route path="/AboutUs" element={<AboutUs />} />
       <Route path="/Eventos" element={<Eventos />} />
@@ -45,9 +45,12 @@ export const router = createBrowserRouter(
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/return" element={<PaymentReturn />} />
       <Route path="/returnPage" element={<ReturnPage />} />
-      <Route path="/register" element={<RegisterPage/>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/private" element={<PrivatePage/>} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/private" element={<PrivatePage />} />
+      <Route path="/tarifas" element={<Tarifas />} />
+      <Route path="/tarifas/:tipo" element={<Tarifas />} /> // importante para detectar el tipo
+
     </Route>
   )
 );

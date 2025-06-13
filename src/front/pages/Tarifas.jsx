4d6c162dde31832e0331824a7903bb35f75d99ff
Cere <c_cere2909@hotmail.com>
2025-06-13 10:49:31 +0000

    modificado

--- src/front/pages/Tarifas.jsx
+++ src/front/pages/Tarifas.jsx
@@ -7,34 +7,6 @@ const Tarifas = () => {
   const { tipo } = useParams();
   const [eleccionSeleccionada, setEleccionSeleccionada] = useState(null);
 
-    const opciones = [
-        {
-            nombre: "Tarifa Basic",
-            descripcion:
-                "Ideal para quienes quieren empezar a moverse. Incluye planes básicos de entrenamiento con tablas de ejercicios estructuradas para todos los niveles.",
-            imagenes: [
-                "https://img.freepik.com/foto-gratis/peso-saludable-cuidado-masculino-atletico_1139-695.jpg?uid=R94462527&ga=GA1.1.2118358263.1748545776&semt=ais_hybrid&w=740"
-            ]
-        },
-        {
-            nombre: "Tarifa Premium",
-            descripcion:
-                "Pensada para quienes buscan un acompañamiento real. Incluye planes de entrenamiento guiados por un entrenador o asesoramiento nutricional personalizado con un nutricionista profesional.",
-            imagenes: [
-                "https://img.freepik.com/foto-gratis/mujer-joven-cinta-metrica-cocina_1303-24778.jpg?uid=R94462527&ga=GA1.1.2118358263.1748545776&semt=ais_hybrid&w=740",
-                "https://img.freepik.com/foto-gratis/pareja-gimnasio_1303-5541.jpg?uid=R94462527&ga=GA1.1.2118358263.1748545776&semt=ais_hybrid&w=740"
-            ]
-        },
-        {
-            nombre: "Tarifa DMPC",
-            descripcion:
-                "La experiencia completa. Accede a todos los planes de entrenamiento y nutrición, con seguimiento personalizado por parte de nuestro equipo de expertos. Todo en uno, sin límites.",
-            imagenes: [
-                "https://img.freepik.com/foto-gratis/mujeres-comida-saludable-tiro-medio_23-2149894948.jpg?uid=R94462527&ga=GA1.1.2118358263.1748545776&semt=ais_hybrid&w=740"
-            ]
-        }
-    ];
-
   const opciones = [
     {
       id: "basic",

2016c831e5970c422694b3db1f51ef3125de2a39
Cere <c_cere2909@hotmail.com>
2025-06-16 11:29:26 +0000

    modificado

--- src/front/pages/User.jsx
+++ src/front/pages/User.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../../styles/User.css";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import { Link } from "react-router-dom";
 
 
 const User = () => {

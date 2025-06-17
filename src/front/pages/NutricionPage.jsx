2f8c2616081659775b2c25ee9ff781b98b68b02f
Cere <c_cere2909@hotmail.com>
2025-06-17 10:22:06 +0000

    modificado

--- src/front/pages/NutricionPage.jsx
+++ src/front/pages/NutricionPage.jsx
@@ -40,11 +40,11 @@ const NutricionPage = () => {
 
       <section className="beneficios my-5">
         <h2 className="text-center subtittle mb-4">¿Por qué elegirnos?</h2>
-        <ul className="list-group list-group-flush caja-bot">
-          <li className="list-group-item">🍏 Asesoramiento profesional</li>
-          <li className="list-group-item">🧬 Ejercicios adaptadas a tu metabolismo</li>
-          <li className="list-group-item">📊 Seguimiento de resultados</li>
-        </ul>
+          <ul className="list-group list-group-flush caja-bot">
+            <li className="list-group-item">🍏 Asesoramiento profesional</li>
+            <li className="list-group-item">🧬 Ejercicios adaptadas a tu metabolismo</li>
+            <li className="list-group-item">📊 Seguimiento de resultados</li>
+          </ul>
       </section>
 
     </div>

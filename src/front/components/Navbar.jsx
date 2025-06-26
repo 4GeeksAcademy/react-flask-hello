a0e204f9b2aa198a961847c889c1d2887db2537f
Jose M. Pinto <josemipinton@gmail.com>
2025-06-26 22:03:43 +0000

    navbar-part-1

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,18 +1,48 @@
 import { Link } from "react-router-dom";
+import { navLinks } from "../utils/navLinks";
+import LogoNavbar from "../assets/img/LogoNavbar.svg";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="container mt-3">
+      		<div className="navbar navbar-expand-md bg-white rounded shadow-sm px-4 py-2">
+				<Link className="navbar-brand" href="#" to="/">
+					<img src={LogoNavbar} alt="Cloudtech" className="Cloudtech-navbar-logo"/>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="hiddencanvas"
+					data-bs-target="#hiddencanvasNavbar"
+					aria-controls="hiddencanvasNavbar"
+					>	
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+			<div className="collapse navbar-collaps justify-content-end" id="hiddencanvasNavbar">
+				<ul className="navbar-nav align-items-center gap-2">
+					<li className="nav-item">
+						<Link className="nav-link custom-hover" to="/">Inicio</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link custom-hover" to="/nosotros">Nosotros</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link custom-hover" to="/">Servcios</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link custom-hover" to="/" >Inicio</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link custom-hover" to="/" >Inicio</Link>
+					</li>
+				</ul>
+			</div>
 			</div>
 		</nav>
 	);
---
+++ src/front/utils/navLinks.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+    {to: "/", label: "Inicio"},
+    {to: "/nosotros", label: "Nosotros"},
+    {to: "/servicios", label: "Proyectos"},
+    {to: "/contacto", label: "Contactanos"},
+];

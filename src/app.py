cbe24792e52856fa139fc20a024e77ab7be6176d
zoscra <jmrg.trabajo@gmail.com>
2025-07-17 20:58:51 +0000

    Models y rutas basicas

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -28,8 +29,13 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config["JWT_SECRET_KEY"] = os.getenv("TOKEN_KEY")
+
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
+
+jwt = JWTManager(app)
 
 # add the admin
 setup_admin(app)

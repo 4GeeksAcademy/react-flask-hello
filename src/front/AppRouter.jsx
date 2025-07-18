2dd4bfc9a4670c77ce54c813c42d412311677fa5
daviidgodino <david.godino@hotmail.com>
2025-07-18 00:12:10 +0000

    login y register

---
+++ src/front/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};

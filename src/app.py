df4864d1cf85fa6907ba15c5d2b848cb1c154686
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-07-19 20:38:09 +0000

    [Se crea endpoint para registrar nuevo vehiculo] app.py: Se escribe endpoint, con condicionales y demas lineas de codigo para crear un nuevo vehiculo asociado a un usuario existente debe enviarse un objeto python con los campos indicados en la tabla Vehiculos del archivo models.py

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db, User, RolEnum
+from api.models import db, User, RolEnum, Vehiculos
 
 from api.routes import api
 from api.admin import setup_admin
@@ -136,6 +136,38 @@ def login():
 
     access_token = create_access_token(identity=user.email)
     return jsonify({'msg': 'ok', 'token': access_token}), 200
+
+
+#ENDPOINT PARA CREAR VEHICULOS
+
+@app.route('/crear_vehiculo', methods = ['POST'])
+def crear_vehiculo():
+    body = request.get_json(silent = True)
+    if body is None:
+        return jsonify({'msg': 'debes enviar informacion del vehiculo en el body'}), 400
+    if 'matricula' not in body:
+        return jsonify({'msg': 'debes enviar la matricula del vehiculo'}), 400
+    if 'marca' not in body:
+        return jsonify({'msg': 'debes enviar la marca del vehiculo'}), 400
+    if 'modelo' not in body:
+        return jsonify({'msg': 'debes enviar el modelo del vehiculo'}), 400
+    if 'year' not in body:
+        return jsonify({'msg': 'debes enviar el año del vehiculo'}), 400
+    if 'user_id' not in body:
+        return jsonify({'msg': 'Debes enviar el Id de un usuario existente'})
+
+    new_car = Vehiculos()
+    new_car.matricula = body['matricula']
+    new_car.marca = body['marca']
+    new_car.modelo = body['modelo']
+    new_car.year = body['year']
+    new_car.user_id = body['user_id']
+
+    db.session.add(new_car)
+    db.session.commit()
+    return jsonify({'msg': 'ok', 'Vehiculo': new_car.serialize()})
+
+    
 
 
 # this only runs if `$ python src/main.py` is executed

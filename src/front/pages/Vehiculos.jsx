1491d4457845f627e47724eed5e97de5f627be23
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-07-19 03:55:13 +0000

    [Creacion de pagina de para mosstrar vehiculos] Vehiculos.jsx: Se creo la estrcutura base de la pagina para mostrar vehiculos por usuario routes.py: Se creo ruta que lleva hasta la pagina /vehiculos

---
+++ src/front/pages/Vehiculos.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react"
+import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Navbar } from "../components/Navbar"
+
+export const Vehiculos = () => {
+
+    return (
+    <div>
+      <Navbar />
+      <h1>ESTA ES LA SECCION PARA VEHICULOS</h1>
+    </div>
+    )
+
+}; 

e7d4bcee18a18f0a3beaf8409c12c3beae569510
Jose M. Pinto <josemipinton@gmail.com>
2025-07-20 00:29:00 +0000

    translation whatsapp button

--- src/front/components/WhatsappButton.jsx
+++ src/front/components/WhatsappButton.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import LogoWhats from "../assets/img/LogoWhats.svg"
 import { FloatingWhatsApp } from "react-floating-whatsapp";
+import { useTranslation } from "react-i18next";
 
 export default function WhatsappButton() {
+    const { t } = useTranslation();
 
     return (
 
         <FloatingWhatsApp
             phoneNumber="593995117464"
-            accountName="CloudTech Sistemas"
-            statusMessage="Normalmente responde en 1 hora"
-            chatMessage="¡Hola! 👋 ¿Cómo podemos ayudarte?"
-            placeholder="Escribe tu mensaje"
             avatar={LogoWhats}
+
+            accountName={t('whatsapp.accountName')}
+            statusMessage={t('whatsapp.statusMessage')}
+            chatMessage={t('whatsapp.chatMessage')}
+            placeholder={t('whatsapp.placeholder')}
+            
         />
     );
 };

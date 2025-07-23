a92ea0b6ea0a494e495e7e17ed2fedeaa85398a9
AdrianBene <adrianbeneroso@hotmail.com>
2025-07-23 11:01:59 +0000

    color login

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -76,22 +76,29 @@ export default function Navbar() {
 
 
             <div class="relative ml-3">
+              <Link to= "/Login">
               <div>
                 <button id="user-menu-button" type="button" aria-expanded="false" aria-haspopup="true" class="relative flex rounded-full bg-indigo-700 text-sm focus:outline-hidden focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:ring-offset-gray-800">
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Menu de usuario</span>
-                  <img onClick={() => setview(!view)} src="https://cdn-icons-png.flaticon.com/512/6681/6681204.png" alt="" class="size-8 w-12 rounded-full" />
+                  <img onClick={() => setview(!view)} src="https://cdn-icons-png.flaticon.com/512/6681/6681204.png"  alt="" class="size-8 w-12 rounded-full" />
                 </button>
               </div>
+              </Link>
+              
 
               <div role="menu" tabindex="-1" aria-labelledby="user-menu-button" aria-orientation="vertical" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden">
                 {
                   view && (
                     <>
-                      <a id="user-menu-item-0" role="menuitem" href="#" tabindex="-1" class="block px-4 py-2 text-sm text-purple-900 hover:bg-orange-200">Login</a>
+                    <Link to ="/Login">
+                     <a id="user-menu-item-0" role="menuitem" href="/Login" tabindex="-1" class="block px-4 py-2 text-sm text-purple-900 hover:bg-orange-200">Login</a>
+                    </Link>
+                     
                       <a id="user-menu-item-1" role="menuitem" href="#" tabindex="-1" class="block px-4 py-2 text-sm text-purple-900 hover:bg-orange-200">Ajustes</a>
                       <a id="user-menu-item-2" role="menuitem" href="#" tabindex="-1" class="block px-4 py-2 text-sm text-purple-900 hover:bg-orange-200">Cerrar sesión</a>
                     </>
+
                   )
                 }
               </div>

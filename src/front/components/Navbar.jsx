43423692a3fdb11531b9532d50034d19d68c7ea6
SergioSanz94x <sergiosanz094@gmail.com>
2025-07-25 18:32:14 +0000

    ¨Navbar¨

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
+  { name: 'My events', href: '#', current: false },
+  { name: 'Public events', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
 ]
 
@@ -12,7 +12,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export function Navbar() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">

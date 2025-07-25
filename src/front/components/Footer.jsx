d9bab8fc3ae32cd071e8fe38c06f4a92060ec8cb
SergioSanz94x <sergiosanz094@gmail.com>
2025-07-25 17:04:33 +0000

    Ultimo cambio login

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => (
 		</p>
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="http://www.4geeksacademy.com">Hector, Ismain, Sergio</a>
 		</p>
 	</footer>
 );

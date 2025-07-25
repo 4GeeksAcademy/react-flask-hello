fa174ce41b8698e1d59334057638712118d4058c
SergioSanz94x <sergiosanz094@gmail.com>
2025-07-25 18:59:42 +0000

    ¨Navbar¨

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -69,8 +69,8 @@ export function Navbar() {
                 <span className="sr-only">Open user menu</span>
                 <img
                   alt=""
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  className="size-8 rounded-full"
+                  src="https://media.licdn.com/dms/image/v2/D4E0BAQEcO3fgdcSiEg/company-logo_200_200/company-logo_200_200/0/1688681342958/4geeksacademyes_logo?e=2147483647&v=beta&t=kGELw1vfpMAZy4w_xBhs1Hz5SYKBO35m7NI9bo3E2Sg"
+                  className="size-3 rounded-full"
                 />
               </MenuButton>
 

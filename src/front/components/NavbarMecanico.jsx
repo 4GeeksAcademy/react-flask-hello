2f86a9e41ff8444754619c1e3ce3e30c8d22e93c
Andrea Nolasco <ant.andreansc@gmail.com>
2025-07-26 03:58:58 +0000

    Se agregan Navbar de User y Mecanico

---
+++ src/front/components/NavbarMecanico.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+
+export const NavbarMecanico = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark shadow-sm mt-2 ms-2 me-2 px-3" style={{ backgroundColor: '#003366', borderRadius: '15px' }}>
+      <div className="container-fluid">
+        <Link className="navbar-brand fw-bold" to="/">
+          AutoTekc
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav align-items-center">
+            
+            <li className="nav-item">
+              <Link className="nav-link" to="/services">Órdenes de trabajo</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Notificaciones</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="btn btn-info ms-3" to="/login">LogOut</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};

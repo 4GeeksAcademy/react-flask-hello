bfd442b82ea41b09970654250deaae48f3e2e56d
alexmararg <alexmar.informatic@gmail.com>
2025-07-26 17:34:43 +0000

    Change Front 7

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 export const Footer = () => {
+  /*const handleGoBack = () => {
+    window.history.back();
+  };*/ /*Es un efecto rallante y lo guardo por si acaso. Alexis*/
   return (
     <footer
       style={{
-        backgroundColor: "white",
-        color: "#000",
+        /*Un pequeño efecto de fondo y un ajuste en el tamaño. Alexis*/
+        background: "linear-gradient(to left, #22b455, #1dd1a1, #22b455)",
+        backgroundSize: "200%",
+        transition: "0.3s linear",
+        minHeight: "6.6vh",
+        color: "#B7FF00",
+        /*Los Cambios finalizan aqui*/
         textAlign: "center",
         padding: "1rem",
         marginTop: "auto",
@@ -14,7 +22,7 @@ export const Footer = () => {
       }}
     >
       <p style={{ margin: 0, fontSize: "1rem" }}>
-        ahorra tu dinero fácilmente
+        Ahorra tu dinero fácilmente
       </p>
     </footer>
   );

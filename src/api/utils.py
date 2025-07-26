ef18e005fa8d1dacad846a558a8b404740abc578
alexmararg <alexmar.informatic@gmail.com>
2025-07-26 18:41:08 +0000

    Page API Backend 8

--- src/api/utils.py
+++ src/api/utils.py
@@ -33,8 +33,8 @@ def generate_sitemap(app):
     links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
     return """
         <div style="text-align: center;">
-        <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
-        <h1>Rigo welcomes you to your API!!</h1>
+        <img style="max-height: 80px" src='https://api.zoviz.com/lfp?b=K2NLr2r0IWQZfbqE&f=btCSiQWhf6m4&d=1'""" 'https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' """ />
+        <h1>Mo'money welcomes you to your API!!</h1>
         <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
         <p>Start working on your project by following the <a href="https://start.4geeksacademy.com/starters/full-stack" target="_blank">Quick Start</a></p>
         <p align="justify" style="padding: 1.8rem">Aquí te explico de forma sencilla cómo funciona la API de usuarios desarrollada con Flask y Python:<br>

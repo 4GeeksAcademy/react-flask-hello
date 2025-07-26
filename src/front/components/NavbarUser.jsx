9bf1c1978eb8eeb1ccff1e1f52734e8c439f403d
Andrea Nolasco <ant.andreansc@gmail.com>
2025-07-26 04:17:56 +0000

    Correccion de LogOut

--- src/front/components/NavbarUser.jsx
+++ src/front/components/NavbarUser.jsx
@@ -32,7 +32,7 @@ export const NavbarUser = () => {
               <Link className="nav-link" to="/about">Perfil</Link>
             </li>
             <li className="nav-item">
-              <Link className="btn btn-info ms-3" to="/login">LogOut</Link>
+              <Link className="btn btn-info ms-3" to="/logout">LogOut</Link>
             </li>
           </ul>
         </div>

79ac0ec314d067035115e43f3eaba34f37cacf5e
SergioSanz94x <sergiosanz094@gmail.com>
2025-07-31 11:04:34 +0000

    crear evento listo

--- src/front/pages/CreateEvent.jsx
+++ src/front/pages/CreateEvent.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function CreateEvent() {
   const [imagePreview, setImagePreview] = useState("/Knect-logo.png");
@@ -36,8 +37,20 @@ export function CreateEvent() {
     // Esto es para luego conectar la logica de supa
   };
 
+  const navigate = useNavigate();
+
+  const rutaVistaHome = () => {
+    navigate("/vistahome");
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="mb-4 flex justify-center">
+        <button
+          onClick={rutaVistaHome}
+          className="mb-1 px-4 py-2 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400"> Volver a home
+        </button>
+      </div>
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
           <form onSubmit={handleSubmit} className="max-w-md mx-auto">
@@ -51,14 +64,12 @@ export function CreateEvent() {
               </div>
               <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
                 <h2 className="leading-relaxed">Crear un evento</h2>
-                <p className="text-sm text-gray-500 font-normal leading-relaxed">
-                  Completa el siguiente formulario.
+                <p className="text-sm text-gray-500 font-normal leading-relaxed"> Completa el siguiente formulario.
                 </p>
               </div>
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 mb-1">
-                Imagen principal del evento
+              <label className="block text-sm font-medium text-gray-700 mb-1"> Imagen principal del evento
               </label>
               <input
                 type="file"
@@ -109,7 +120,7 @@ export function CreateEvent() {
               <input
                 type="time"
                 name="time"
-                step="300"         
+                step="300"
                 value={formData.time}
                 onChange={handleChange}
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
@@ -133,15 +144,13 @@ export function CreateEvent() {
                 name="visibility"
                 value={formData.visibility}
                 onChange={handleChange}
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-              >
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2">
                 <option value="public">Público</option>
                 <option value="private">Privado</option>
               </select>
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Máximo de asistentes (opcional)
+              <label className="block text-sm font-medium text-gray-700"> Máximo de asistentes (opcional)
               </label>
               <input
                 type="number"
@@ -154,13 +163,11 @@ export function CreateEvent() {
             <div className="flex justify-end space-x-4">
               <button
                 type="button"
-                className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300"
-              >Cancelar
+                className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300"> Cancelar
               </button>
               <button
                 type="submit"
-                className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
-              >Crear evento
+                className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"> Crear evento
               </button>
             </div>
           </form>

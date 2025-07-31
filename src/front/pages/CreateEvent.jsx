0fadbb5b7d8cef6fed0ec55c79fe7d68c1706e41
SergioSanz94x <sergiosanz094@gmail.com>
2025-07-31 11:40:13 +0000

    formulario final

--- src/front/pages/CreateEvent.jsx
+++ src/front/pages/CreateEvent.jsx
@@ -51,22 +51,24 @@ export function CreateEvent() {
           className="mb-1 px-4 py-2 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400"> Volver a home
         </button>
       </div>
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
-          <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-            <div className="flex items-center space-x-5 mb-6">
-              <div className="h-full w-full bg-gray-200 rounded-full overflow-hidden">
+      <div className="relative py-3 sm:max-w-5xl sm:mx-auto">
+        <div className="relative px-4 py-8 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-8">
+          <form onSubmit={handleSubmit} className="max-w-2xl mx-auto">
+            <div className="mb-8 text-center">
+              <h2 className="text-3xl font-bold text-gray-800 leading-relaxed"> Crear un evento</h2>
+            </div>
+            <div className="flex justify-center mb-2">
+              <div className="h-35 w-35 bg-gray-200 rounded-full overflow-hidden flex items-center justify-center">
                 <img
                   src={imagePreview}
                   alt="Imagen del evento"
                   className="h-full w-full object-cover"
                 />
               </div>
-              <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
-                <h2 className="leading-relaxed">Crear un evento</h2>
-                <p className="text-sm text-gray-500 font-normal leading-relaxed"> Completa el siguiente formulario.
-                </p>
-              </div>
+            </div>
+            <div className="mb-6 text-center">
+              <p className="text-base text-gray-500 font-normal leading-relaxed"> Completa el siguiente formulario.
+              </p>
             </div>
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700 mb-1"> Imagen principal del evento
@@ -90,11 +92,10 @@ export function CreateEvent() {
                 value={formData.title}
                 onChange={handleChange}
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                required
-              />
+                required/>
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Descripción</label>
+              <label className="block text-sm font-medium text-gray-700"> Descripción </label>
               <textarea
                 name="description"
                 value={formData.description}
@@ -105,15 +106,14 @@ export function CreateEvent() {
               ></textarea>
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Fecha</label>
+              <label className="block text-sm font-medium text-gray-700"> Fecha </label>
               <input
                 type="date"
                 name="date"
                 value={formData.date}
                 onChange={handleChange}
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                required
-              />
+                required/>
             </div>
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700">Hora</label>
@@ -124,8 +124,7 @@ export function CreateEvent() {
                 value={formData.time}
                 onChange={handleChange}
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                required
-              />
+                required/>
             </div>
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700">Ubicación o enlace</label>
@@ -135,8 +134,7 @@ export function CreateEvent() {
                 value={formData.location}
                 onChange={handleChange}
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                required
-              />
+                required/>
             </div>
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700">Visibilidad</label>

c3822c2e40f3c00bc5ba7f1e42944d0bfd672f8f
HHectorAlonso <Hector_alonso_martin@hotmail.es>
2025-08-01 16:22:25 +0000

    pagina not found

--- src/front/pages/NotFound.jsx
+++ src/front/pages/NotFound.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { VistaHome } from "../components/VistaHome";
+import { div } from "framer-motion/client";
 
 export const NotFound = () => {
 
@@ -8,8 +9,34 @@ export const NotFound = () => {
   const rutaVistaHome = () => {
     navigate("/vistahome");
   };
+  const mostrarJuego =()=> {
+    const juego = document.getElementById("juego");
+    if(juego) {
+      juego.classList.toggle("hidden");
+    }
+  }
 
   return (
-    <button className="text-black bg-yellow-700" onClick={rutaVistaHome}>Home</button>
+    <div>
+    <div className="flex justify-center">
+      <h1 className="m-4 text-blue-600 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">404 No encontrado</h1>
+    </div>
+    <div className="flex justify-center">
+      <p>Pero no te preocupes, no nos hemos ido de vacaciones, solo esta tardando un poco mas en cargar</p>
+    </div>
+      <br />
+    <div className="flex justify-center">
+    </div>
+      <div className="text-center justify-center">
+      <p className="m-3">mientras tanto, te ofrecemos una partidita al pacman </p>
+      <p>si quieres jugar pulsa el boton</p>
+      <button onClick={()=>mostrarJuego()} >JUGAR</button>
+
+      <div id="juego" className="flex justify-center hidden mt-4 p-4 bg-gray-100 rounded">
+        <iframe src="./404_not_found/index.html" width="640" height="480" frameborder="0"></iframe>
+      </div >
+      </div>
+      <button className="m-10 text-black bg-yellow-700" onClick={rutaVistaHome}>Home</button>
+    </div>
   );
 };

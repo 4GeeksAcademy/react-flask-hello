419e02a4a34cca9f7538c89c334727c31bafe479
Sebas-RS <sebas.r.s18@gmail.com>
2025-08-04 08:24:19 +0000

    backen terminado con google maps

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,6 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask_cors import CORS
 from flask import Blueprint
 from .user import user_bp
 from .events import events_bp

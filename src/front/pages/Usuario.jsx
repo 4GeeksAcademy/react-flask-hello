c032871091e648a5e66f7af5d62e0e7828ee8ba7
HHectorAlonso <Hector_alonso_martin@hotmail.es>
2025-08-08 16:37:38 +0000

    .

--- src/front/pages/Usuario.jsx
+++ src/front/pages/Usuario.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { UserRoleSelector } from "../components/UserRoleSelector"; // ✅ Importamos el selector de rol
+import { User } from "../components/User"
 
 export const Usuario = () => {
   const navigate = useNavigate();
@@ -10,11 +11,11 @@ export const Usuario = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6 p-6 bg-gray-100">
-      <h1 className="text-2xl font-bold text-gray-800">Bienvenido al Panel de Usuario</h1>
+      <h1 className="text-2xl font-bold text-gray-800">Panel de Usuario</h1>
 
       {/* El rolesito */}
       <div className="w-full max-w-xs">
-        <UserRoleSelector />
+        <User />
       </div>
 
       {/* Botón para ir a la VistaHome */}
@@ -22,7 +23,7 @@ export const Usuario = () => {
         className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded shadow"
         onClick={rutaVistaHome}
       >
-        Ir a VistaHome
+        Ir a Home
       </button>
     </div>
   );

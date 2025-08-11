9bcaf7e6918a6ebff0a018cb4bba4ad53b543bc4
SergioSanz94x <sergiosanz094@gmail.com>
2025-08-11 17:40:58 +0000

    fetch de user bien

--- src/front/pages/Callback.jsx
+++ src/front/pages/Callback.jsx
@@ -1,1 +1,2 @@
-// Es la ruta que se envia al usuario una vez se a partir de ahi de redirecciona intentar con localhost para obtener token
+// Es la ruta que se envia al usuario una vez  a partir de ahi que redirecciona intentar con localhost para obtener token
+

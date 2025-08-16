5799e9d07777e9994a77d0188cdc017fa00b32d0
Silvia-LC <lopezsilvia36@gmail.com>
2025-08-16 19:17:50 +0000

    estilos forms-home-register-recpassword

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar({showDrowpdown, setShowDrowpdown}) {
 
   return (
     <nav className="bg-gray-900">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className=" navbar-bg mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button type="button" aria-controls="mobile-menu" aria-expanded="false" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">

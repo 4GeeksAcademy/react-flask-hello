fadd591502ca57438fd5ca0be054a7376ee99350
SergioSanz94x <sergiosanz094@gmail.com>
2025-08-17 14:34:05 +0000

    muestra todos los eventos y componente card arreglado

--- src/front/components/Card.jsx
+++ src/front/components/Card.jsx
@@ -1,13 +1,13 @@
-export const Card = ({d}) => {
+export const Card = ({ d }) => {
   return (
     <article className="dest-card">
-                <div className="thumb">
-                  <img src={d.img} alt={d.titulo} className="thumb-img" />
-                </div>
-                <div className="meta">
-                  <h3 className="title">{d.titulo}</h3>
-                  <p className="country">{d.pais}</p>
-                </div>
-              </article>
+      <div className="thumb">
+        <img src={d.img} alt={d.titulo} className="thumb-img" />
+      </div>
+      <div className="meta">
+        <h3 className="title">{d.titulo}</h3>
+        <p className="country">{d.pais}</p>
+      </div>
+    </article>
   );
 }

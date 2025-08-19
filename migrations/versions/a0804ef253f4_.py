d021b9619424ff03d73ce7d1c76923ea5ee3a3bd
AdrianBene <adrianbeneroso@hotmail.com>
2025-08-19 21:24:48 +0000

    migrate

--- migrations/versions/a0804ef253f4_.py
+++ migrations/versions/a0804ef253f4_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: b07da2e750df
+Revision ID: a0804ef253f4
 Revises: 
-Create Date: 2025-08-19 21:00:25.203060
+Create Date: 2025-08-19 21:24:09.692846
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b07da2e750df'
+revision = 'a0804ef253f4'
 down_revision = None
 branch_labels = None
 depends_on = None

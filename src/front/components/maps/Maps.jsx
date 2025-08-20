34d1db915515fa6100d71159334b408a7140fb87
Betoluchenco <63628143+Betoluchenco@users.noreply.github.com>
2025-08-20 10:02:42 +0000

    valencia ubicacion

--- src/front/components/maps/Maps.jsx
+++ src/front/components/maps/Maps.jsx
@@ -1,18 +1,20 @@
-import { APIProvider, Map, useApiIsLoaded } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, useApiIsLoaded, Marker } from "@vis.gl/react-google-maps";
 
 const API_KEY = import.meta.env.VITE_API_KEY_MAPS;
 
 function MapInner() {
   const isLoaded = useApiIsLoaded();
+  const position = { lat:39.47,lng:-0.37}
   if (!isLoaded) return <div style={{padding: 12}}>Valencia</div>;
   return (
     <Map
       style={{ width: "100%", height: "100%" }}
-      defaultCenter={{ lat: 22.54992, lng: 0 }}
-      defaultZoom={3}
+      defaultCenter={position}
+      defaultZoom={13}
       gestureHandling="greedy"
       disableDefaultUI={false}
-    />
+    ><Marker position={position}></Marker>
+    </Map>
   );
 }
 

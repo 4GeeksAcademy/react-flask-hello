2d3316242e01cb709a63c3676288a9afbc899d77
Galeon-91 <ismainperera91@gmail.com>
2025-08-25 18:25:31 +0000

    Cambios en Lista Eventos + store.jsx + ID_Evento con Hori

--- src/front/pages/ID_Evento.jsx
+++ src/front/pages/ID_Evento.jsx
@@ -3,60 +3,35 @@ import React, { useEffect, useState } from 'react';
 import { backendUrl } from '../utils/Config';
 import { CardEvento } from "../components/CardEvento/CardEvento";
 import useGlobalReducer from "../hooks/useGlobalReducer";
-import { useParams } from "react-router-dom"; // <-- añadido
+import { useParams } from "react-router-dom";
 
 export const ID_Evento = () => {
-        const {event_id} = useParams();
+        const { event_id } = useParams();
+        const { dispatch, store } = useGlobalReducer();
+        const [evento, setEvento] = useState(null);
 
-
-        const { dispatch, store } = useGlobalReducer()
-        console.log(store)
+        const findEvent = store.eventos.find(event => event.id == event_id)
+        console.log(findEvent)
 
         useEffect(() => {
-                const eventoUsuarioLogin = async () => {
-                        const tokenEvento = localStorage.getItem("token");
-                        try {
-                                const respuesta = await fetch(backendUrl + `api/events/${event_id}`, {
-                                        method: 'GET',
-                                        headers: {
-                                                'Authorization': `Bearer ${tokenEvento}`,
-                                                'Content-Type': 'application/json'
-                                        }
-                                });
-
-                                const data = await respuesta.json();
-                                console.log(data)
-
-                                if (respuesta.ok) {
-                                        dispatch({
-                                                type: "ID_Evento",
-                                                payload: data.response
-                                        })
-                                        notifySuccess("Aquí está tu listado de eventos");
-                                } else {
-                                        notifyError(data.message || "No tienes eventos disponibles");
-                                }
-                        } catch (error) {
-                                notifyError("Error al cargar los eventos");
-                        }
-                };
-
-                eventoUsuarioLogin();
+                if (!findEvent){
+                        console.log("Evento no encontrado");
+                        getEvent()
+                }
         }, []);
 
-
+        if (!findEvent) {
+                return (
+                        <div>
+                                <h1>Cargando...</h1>
+                        </div>
+                );
+        }
         return (
                 <div>
-                        <h1>Mis eventos</h1>
-                        <section className="grid-cards ">
-                                {store.misEventos.length === 0 ? (
-                                        <p>No tienes eventos creados aún.</p>
-                                ) : (
-                                        store.misEventos.map((evento, index) => (
-                                                <CardEvento key={index} item={evento} isUser={true} />
-                                        ))
-                                )}
-                        </section>
+                        <h1>Evento {findEvent.titulo}</h1>
+                        <img src={findEvent.portada} alt={findEvent.titulo} />
+
                 </div>
         );
 };
--- src/front/pages/ListaEventos.jsx
+++ src/front/pages/ListaEventos.jsx
@@ -3,23 +3,27 @@ import React, { useEffect, useState } from 'react';
 import { backendUrl } from '../utils/Config';
 import { CardEvento } from "../components/CardEvento/CardEvento";
 import { Link } from "react-router-dom"; // <-- añadido
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const ListaEventos = () => {
-  const [eventos, setEventos] = useState([]);
+  const { dispatch, store } = useGlobalReducer();
 
   useEffect(() => {
     const listadoEventos = async () => {
       try {
         const respuesta = await fetch(backendUrl + `events`);
         const data = await respuesta.json();
-        console.log(data);
 
         if (!respuesta.ok) {
           notifyError("Error al cargar los eventos");
           return;
         }
 
-        setEventos(data.response);
+        dispatch({
+          type: "setEvents",
+          payload: data.response
+        })
+
       } catch (error) {
         notifyError("Error al cargar los eventos");
       }
@@ -27,14 +31,15 @@ export const ListaEventos = () => {
     listadoEventos();
   }, []);
 
+
   return (
     <div>
       <h1>Lista de Eventos</h1>
       <section className="grid-cards">
-        {eventos.length === 0 ? (
+        {store.eventos.length === 0 ? (
           <p>No hay eventos creados aún.</p>
         ) : (
-          eventos.map((evento) => (
+          store.eventos.map((evento) => (
             <Link key={evento.id} to={`/evento/${evento.id}`} className="card-link">
               <CardEvento item={evento} isUser={false} />
             </Link>

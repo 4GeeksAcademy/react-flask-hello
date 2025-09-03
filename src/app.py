3e5b8227b0bba06e9c09ec7f39d8146f50fac52e
Kelvin Lawler <kelvinlawler96@icloud.com>
2025-09-03 18:09:16 +0000

    Tesr Users Created

--- src/app.py
+++ src/app.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate

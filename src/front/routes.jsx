73050255f4d5997d2f1543fe8a0ee4b46c834094
Kelvin Lawler <kelvinlawler96@icloud.com>
2025-09-06 18:12:52 +0000

    updated Landing page

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -22,11 +22,11 @@ export const router = createBrowserRouter(
     // Root Route: All navigation will start from here.
     <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
-      {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-      <Route path="/" element={<Home />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
-      <Route path="/demo" element={<Demo />} />
+      {/* Show Landing page at root */}
+      <Route index element={<Landing />} />
+      <Route path="home" element={<Home />} />
+      <Route path="single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
+      <Route path="demo" element={<Demo />} />
     </Route>
   )
 );

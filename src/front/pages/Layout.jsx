baeb1e975993c94fb70b593f8d0f96a1621c71a3
DevKeria <keriawiley@yahoo.com>
2025-09-08 18:52:36 +0000

    landing page

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,15 +1,14 @@
-import { Outlet } from "react-router-dom/dist"
-import ScrollToTop from "../components/ScrollToTop"
-import { Navbar } from "../components/Navbar"
-import { Footer } from "../components/Footer"
+import { Outlet } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
-// Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
-    return (
-        <ScrollToTop>
-            <Navbar />
-                <Outlet />
-            <Footer />
-        </ScrollToTop>
-    )
-}
+  return (
+    <ScrollToTop>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </ScrollToTop>
+  );
+};

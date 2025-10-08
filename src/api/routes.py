c0228757bbfcde974d7261ee1666e24df5667ebf
Facundo  Bravo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-10-08 22:24:49 +0000

    finalizados los endpoints de friend, y casi finalizados los de todo, falta el de editar y queda lista esta seccion

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Friend
+from api.models import db, User, Friend, Todo, GroupTodo
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from sqlalchemy import and_
@@ -22,7 +22,7 @@ def get_users():
     return jsonify([user.serialize() for user in users]), 200
 
 
-#----------------GET ALL USERS----------------------------------#
+#----------------GET SPECIFIC USER----------------------------------#
 
 @api.route('/user/<int:id>', methods=['GET'])
 def get_user(id):
@@ -92,7 +92,7 @@ def edit_user(id):
 
 
 
-    return jsonify({"user": user.serialize()}), 200
+    return jsonify(user.serialize()), 200
 
 
 
@@ -154,7 +154,7 @@ def get_friendship():
         return jsonify({"error": "none user has been founded with the provided user_to_id"}),400
     
 
-    return jsonify({"friendship": [user_from.user_name, user_to.user_name]}),200
+    return jsonify({"friendship_members": [user_from.user_name, user_to.user_name], "friendship_id": friendship.id}),200
 
 
 #--------------------------CREATE USER FRIENDSHIP--------------------------------------#
@@ -195,6 +195,234 @@ def create_friendship():
     db.session.commit()
 
     return jsonify({"message": f"friendship between {user_from.user_name} and {user_to.user_name} successfully created"}), 200
+
+
+    
+#---------------------------DELETE USER FRIENDSHIP --------------------------------------#
+@api.route('/user/friendship', methods=['DELETE'])
+def delete_friendship():
+    
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "send a body with info to create a friendship"}),400
+    
+    user_from_id = data["user_from_id"]
+    user_to_id = data["user_to_id"]
+
+    if not user_from_id or not user_to_id:
+        return jsonify({"error": "please send the user_to_id and user_from_id to delete the friendship correctly"}),400
+    
+    friendship = Friend.query.filter(and_(Friend.user_from_id == user_from_id, Friend.user_to_id == user_to_id)).first()
+
+    if not friendship:
+        return jsonify({"error": "friendship not founded"}), 400
+
+    db.session.delete(friendship)
+    db.session.commit()
+
+    return jsonify({"message": "friendship deleted successfully"}),200
+
+
+
+#-------------------------------------------FIN CRUD FRIEND --------------------------------------------------------------------------------------------#
+
+
+#------------------------------------------- Todo'S CRUD  --------------------------------------------------------------------------------------------#
+    
+ #-----------------GET ALL USER Todos------------------#
+@api.route('/user/todos', methods=['GET'])
+def get_user_todos():
+
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "send a body with info to create a friendship"}),400
+
+    user_id = data["user_id"]
+
+    if not user_id:
+        return jsonify({"error": "missing user_id field on the body"}),400
+    
+    todos = Todo.query.filter_by(user_id=user_id).all()
+
+    if not todos:
+        return jsonify({"error": "there is no todos for this user_id"}), 400
+    
+    return jsonify([todo.serialize() for todo in todos]),200
+
+
+
+ #-----------------GET UNIQUE USER Todo------------------#
+@api.route('/user/todo', methods=['GET'])
+def get_user_todo():
+    
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "send a body with info to get an user todo"}),400
+    
+    user_id = data["user_id"]
+    todo_id = data["todo_id"]
+    
+    
+    if not user_id and not todo_id:
+        return jsonify({"error": "missing user_id and todo_id field on the body"}),400
+    
+    todo = Todo.query.filter_by(user_id=user_id, id=todo_id).first()
+
+    if not todo:
+        return jsonify({"error": "none todo has been founded with the user_id or todo_id"}), 400
+    
+    return jsonify(todo.serialize())
+    
+
+ #-----------------CREATE UNIQUE USER Todo------------------# 
+@api.route('/user/todo', methods=['POST'])
+def create_user_todo():
+    
+    data = request.get_json()
+    
+    if not data:
+        return jsonify({"error": "send a body with info to create an user todo"}),400
+    
+
+
+    title = data.get("title")
+    description = data.get("description")
+    user_id = data.get("user_id")
+
+    if not title and not user_id:
+
+        return jsonify({"error": "please provide an user_id and a title"}),400
+    
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "there is not user with this user_id"}), 400
+
+    todo = Todo(title=title, user_id=user_id)
+
+    if description:
+        todo.description = description
+
+    db.session.add(todo)
+    db.session.commit()
+
+    return jsonify(todo.serialize())
+
+ #-----------------DELETE UNIQUE USER Todo------------------# 
+
+@api.route('/user/todo', methods=['DELETE'])
+def delete_user_todo():
+        
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"error": "send a body with info to delete an user todo"}),400
+
+        user_id = data.get("user_id")
+        todo_id = data.get("todo_id")
+
+        if not user_id or not todo_id:
+            return jsonify({"error": "user_id and todo_id fields are missing"}), 400
+        
+        user = User.query.get(user_id)
+
+        if not user:
+            return jsonify({"error": "no user was founded with the provided user_id"}),400
+        
+        todo = Todo.query.filter_by(user_id=user_id, id=todo_id).first()
+
+        if not todo:
+            return jsonify({"error": "no todo was founded with the provided todo_id"}),400
+        
+        db.session.delete(todo)
+        db.session.commit()
+
+        return jsonify({"message": f"the todo with the id {todo_id} associated with the user {user_id} was successfully deleted"}), 200
+ 
+
+ #-----------------EDIT UNIQUE USER Todo------------------# 
+@api.route('/user/todo', methods=['PATCH'])
+def edit_user_todo():
+    
+    data = request.get_json()
+
+    if not data:
+         return jsonify({"error": "send a body with info to modify an user todo"}),400
+
+    user_id = data.get("user_id")
+    todo_id = data.get("todo_id")
+
+    if not user_id or not todo_id:
+            return jsonify({"error": "user_id and todo_id fields are missing"}), 400
+    
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "no user was founded with the provided user_id"}),400
+    
+
+    todo = Todo.query.filter_by(user_id=user_id, id=todo_id).first()
+
+    if not todo:
+        return jsonify({"error": "there was not todo founded with the provided user_id and todo_id"}),400
+    
+    title = data.get("title")
+    description = data.get("description")
+
+    if title:
+        todo.title = title
+    if description:
+        todo.description = description
+
+    db.session.commit()
+
+    return jsonify(todo.serialize()), 200
     
 
     
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------FIN CRUD Todo --------------------------------------------------------------------------------------------#
+
+#-------------------------------------------GROUPTODO CRUD --------------------------------------------------------------------------------------------#
+
+#-----------------GET ALL FRIEND Todos------------------#
+@api.route('/friendship/todos', methods=['GET'])
+def get_friendship_todos():
+    pass
+
+   
+ #-----------------GET UNIQUE FRIEND Todo------------------#
+@api.route('/friendship/todo', methods=['GET'])
+def get_friendship_todo():
+    pass   
+
+ #-----------------CREATE UNIQUE FRIEND Todo------------------# 
+@api.route('/friendship/todo', methods=['GET'])
+def create_friendship_todo():
+    pass    
+
+ #-----------------DELETE UNIQUE FRIEND Todo------------------# 
+
+@api.route('/friendship/todo', methods=['GET'])
+def delete_friendship_todo():
+    pass
+
+ #-----------------EDIT UNIQUE FRIEND Todo------------------# 
+@api.route('/friendship/todo', methods=['GET'])
+def edit_friendship_todo():
+    pass
+
+
+

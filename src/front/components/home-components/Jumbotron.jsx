610febe75d80be3f58b47333d5712eda9d2a170f
Facundo  Bravo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-10-10 19:52:52 +0000

    agregadas las dos columnas que distribuiran el contenido dentro del jumbotron

--- src/front/components/home-components/Jumbotron.jsx
+++ src/front/components/home-components/Jumbotron.jsx
@@ -2,9 +2,22 @@ const Jumbotron = () => {
     return (
         <div className="jumbotron bg-light rounded-5 border border-5 border-black overflow-hidden" >
 
-            <img className="object-fit-cover w-100 h-100" src="https://preview.redd.it/is-uchiha-itachi-the-most-favorite-character-in-naruto-v0-ikt8zb3eny6d1.jpeg?width=640&crop=smart&auto=webp&s=b615740fe4ad219da304f7ef024f55b0f0f0b49d"/>
 
-
+            <div className="row h-100">
+                <div class="col-6 bg-danger h-100">
+                        <img className="object-fit-cover" src="https://i.pinimg.com/736x/6f/e2/d3/6fe2d31038a92c02223c31d9dc0bdfca.jpg" />
+                
+                </div>
+                <div class="col-6 bg-danger d-flex">
+                    <div className="d-flex flex-column justify-content-center align-items-start pe-5">
+                        <h1 className="display-3">Welcome buccaneers</h1>
+                        <p>Join Claude, a determined marine, on his journey to reach the legendary island of Tartara.
+                            Every task you and your friends add becomes a mission that brings him closer to shore.
+                            Write your goals. Share the challenge. Reach Tartara.</p>
+                        <button type="button" className="btn btn-primary">Join the mission</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

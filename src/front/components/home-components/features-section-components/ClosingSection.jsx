44ce6324d16479c08f4ba1d48999aeb9d765aeae
Facundo  Bravo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-10-11 11:54:33 +0000

    closing-section completa

--- src/front/components/home-components/features-section-components/ClosingSection.jsx
+++ src/front/components/home-components/features-section-components/ClosingSection.jsx
@@ -2,11 +2,21 @@ const ClosingSection = () => {
 
     return (
         <div className="closing-section back-color-4 rounded-5">
-            <div className="d-flex flex-column">
+            <div className="d-flex flex-column align-items-center p-4 h-100">
                 <div className="closing-img">
+                    <img className="object-fit-cover h-100" src="https://res.cloudinary.com/dra2cr3uw/image/upload/v1760182053/Elio_ejemplo_zqfn5o.png"></img>
+                </div>
+                <div className="closing-text back-color-3 rounded-5 flex-grow-1 w-100 text-center">
+                    <div className="d-flex flex-column justify-content-center align-items-center h-100 px-5">
+
+                        <p className="fs-4">Elio's not just searching for Tartara. He's discovering himself along the way.
+                            Every mission completed brings him closer to home</p>
+                        <button type="button" className="btn button-2-color-2 font-color-1 fw-semibold">
+                            KEEP HELPING ELIO
+                        </button>
+                    </div>
 
                 </div>
-                <div className=""></div>
             </div>
         </div>
     )

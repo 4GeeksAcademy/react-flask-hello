b82a7db923d0db106e1e348b67121c511977d158
dianavrojas <dianavalentinara@gmail.com>
2025-10-18 22:39:46 +0000

    login listo

--- src/front/services/userServices.jsx
+++ src/front/services/userServices.jsx
@@ -1,20 +1,39 @@
 const userServices = {}
 const url = import.meta.env.VITE_BACKEND_URL
-    userServices.register = async(formData)=>{
-        try {
-            const resp = await fetch(url+'/api/register', {
-                method:'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)        
-            })
-            if(!resp.ok) throw new Error('error al registrar usuario')
-        const data = await resp.json()
-        return data
-        } catch (error) {
-            console.log(error)
-        }
-    }
 
-    export default userServices
+userServices.register = async (formData) => {
+  try {
+    const resp = await fetch(url + '/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    if (!resp.ok) throw new Error('error al registrar usuario')
+    const data = await resp.json()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// 👇 Mueve esto FUERA del register
+userServices.login = async (formData) => {
+  try {
+    const resp = await fetch(url + '/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    const data = await resp.json()
+    if (!resp.ok) return data
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default userServices

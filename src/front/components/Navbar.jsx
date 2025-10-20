b88a9570a0602c19f1088326eb94ba7cf2e81b2c
Hun73R717 <headhunter717@gmail.com>
2025-10-20 19:32:55 +0000

    modificacion de navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,17 +3,29 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
+		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid justify-content-end">
+    <a class="navbar-brand" href="#">Products</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Shopping Cart</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">QR Code Generator</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+       <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 	);
 };

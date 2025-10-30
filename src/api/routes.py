47027580631e66c7b6c2ef55520e10c86357955b
1v4nP4 <1v4nP4@proton.me>
2025-10-30 13:49:38 +0000

    endpoint login creado

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from bcrypt import bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from sqlalchemy import select
 
 api = Blueprint('api', __name__)
 
@@ -28,8 +30,8 @@ def handle_register():
 
     body = request.get_json()
 
-    hashed_password = bcrypt.generate_password_hash(
-        body['password']).decode('utf-8')
+    hashed_password = generate_password_hash(
+        body['password'])
 
     new_user = User(role=body['role'], nickname=body['nickname'], nombre=body['nombre'], apellido=body['apellido'], fecha_nacimiento=body['fecha_nacimiento'], email=body['email'],
                     address=body['address'], telefono=body['telefono'], password=body[hashed_password], registro_fecha=body['registro_fecha'], is_active=body['is_active'])
@@ -37,3 +39,39 @@ def handle_register():
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'msg': 'Nuevo usuario creado con exito'}), 201
+
+@api.route('/update', methods=['PUT'])
+@jwt_required()
+def handle_update():
+
+    id=get_jwt_identity()
+    body = request.get_json()
+    hashed_password = generate_password_hash(body['password']).decode('utf-8')
+
+    user=db.session.get(User,id)
+    user.role=body.get('role', user.role)
+    user.nickname=body['nickname']
+    user.nombre=body['nombre']
+    user.apellido=body['apellido'] 
+    user.fecha_nacimiento=body['fecha_nacimiento'] 
+    user.email=body['email']
+    user.address=body['address'] 
+    user.telefono=body['telefono'] 
+    user.password=body[hashed_password] 
+    db.session.commit()
+    return jsonify({'user':user.serialize()})
+   
+@api.route('/login', methods=['POST'])
+def handle_login():
+    body= request.json
+    stm= select(User).where(User.email==body['email'])
+    user=db.session.execute(stm).scalar_one_or_none()
+
+    if not user : 
+        return jsonify({'msg':'email no encontrado'}),404
+    
+    if not check_password_hash(user.password , body['password']):
+        return jsonify({'msg':'email y/o contraseña no valido'}),400
+    
+    token=create_access_token(identity=str(user.id))
+    return jsonify({'user':user.serialize(),'token':token}),200

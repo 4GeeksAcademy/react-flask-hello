ce5784aa47510dabae8b60f8589acd4b62ab4eb9
Juanjo Villarejo Cespedes <jota_villarejo22@hotmail.com>
2025-11-03 08:41:17 +0000

    importado User

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands

8114ba49ac581a546bdb921a72fa339483ac08d5
1v4nP4 <1v4nP4@proton.me>
2025-11-04 12:55:33 +0000

    perfil empezado

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -12,6 +12,7 @@ import { Demo } from "./pages/Demo";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { CreateEditClient } from "./components/CreateEditClient";
+import { PerfilPage } from "./pages/PerfilPage";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -31,6 +32,7 @@ export const router = createBrowserRouter(
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/createeditclient" element={<CreateEditClient/>}/>
+        <Route path="/perfil" element={<PerfilPage/>}/>
       </Route>
     )
 );

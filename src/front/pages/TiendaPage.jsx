564e95b79385cdf60e1e7b38563a916cba14e145
1v4nP4 <1v4nP4@proton.me>
2025-11-07 20:26:50 +0000

    commit

--- src/front/pages/TiendaPage.jsx
+++ src/front/pages/TiendaPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Tienda } from "../components/Tienda";
 
+
 export const TiendaPage = () => {
 
     return(
+        <>
         <Tienda/>
+        </>
     )
 
 }

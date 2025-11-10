966afcf5cb86af57c3c51fd61168e73e30f489b1
1v4nP4 <1v4nP4@proton.me>
2025-11-10 17:49:44 +0000

    stripe agregado

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
-import stripe
+import stripe 
 
 # Load environment variables
 load_dotenv()

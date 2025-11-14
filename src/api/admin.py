14ec2d511b5e23797449fe743e659a056350e4e9
Mnrubn <126007823+Mnrubn@users.noreply.github.com>
2025-11-14 19:03:20 +0000

    reparación

--- src/api/admin.py
+++ src/api/admin.py
@@ -8,7 +8,7 @@ from flask_admin.contrib.sqla import ModelView
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin')
+    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
